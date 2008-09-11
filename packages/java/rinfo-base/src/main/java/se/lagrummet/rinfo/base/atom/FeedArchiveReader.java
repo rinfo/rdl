2b29ae9c6a0177aa197f2f4c66b8b080518ee946
nlm <nlm@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2008-09-11 19:47:09 +0000

    switched to httpclient (4) for url fetching
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@113 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- packages/java/rinfo-base/src/main/java/se/lagrummet/rinfo/base/atom/FeedArchiveReader.java
+++ packages/java/rinfo-base/src/main/java/se/lagrummet/rinfo/base/atom/FeedArchiveReader.java
@@ -3,14 +3,18 @@ package se.lagrummet.rinfo.base.atom;
 import java.util.*;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLEncoder;
 
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import org.apache.abdera.Abdera;
 import org.apache.abdera.i18n.iri.IRI;
@@ -22,6 +26,27 @@ public abstract class FeedArchiveReader {
 
     private final Logger logger = LoggerFactory.getLogger(FeedArchiveReader.class);
 
+    HttpClient httpClient;
+
+    public FeedArchiveReader() {
+        this.httpClient = createClient();
+    }
+
+    public final HttpClient getClient() {
+        return httpClient;
+    }
+
+    /**
+     * Overridable create method for an {@link HttpClient}. This defaults to
+     * {@link DefaultHttpClient} with no special settings.
+     */
+    public HttpClient createClient() {
+        return new DefaultHttpClient();
+    }
+
+    /**
+     * Starts the feed archive climbing.
+     */
     public void readFeed(URL url) throws IOException {
         URL followingUrl = url;
         while (followingUrl != null) {
@@ -33,14 +58,43 @@ public abstract class FeedArchiveReader {
         logger.info("Done.");
     }
 
+    /**
+     * Utility method to use the HttpClient (via {@link createClient}) to open
+     * an URL and get the entity content as an InputStream.
+     *
+     * @return InputStream, or null if the response didn't enclose an entity.
+     */
+    public InputStream getResponseAsInputStream(URL url) throws IOException {
+        return getResponseAsInputStream(url.toString());
+    }
+
+    public InputStream getResponseAsInputStream(String url) throws IOException {
+        HttpGet urlGet = new HttpGet(url);
+        HttpResponse response = getClient().execute(urlGet);
+        HttpEntity entity = response.getEntity();
+        if (entity == null) {
+            return null;
+        }
+        return entity.getContent();
+    }
+
+    /**
+     * <p>Parses the feed and delegates to {@link processFeedPage} for handling.
+     * Then looks for contained archive links to continue climbing (backwards
+     * in time).</p>
+     * <p>Only override this if you need to e.g. stop climbing at a certain
+     * known archive URL.</p>
+     *
+     * @return the previous archive URL, or null to stop climbing.
+     */
     public URL readFeedPage(URL url) throws IOException {
         logger.info("Reading Feed <"+url+"> ...");
         Feed feed;
         URL followingUrl = null;
-        ReadableByteChannel inChannel = Channels.newChannel(url.openStream());
+        InputStream inStream = getResponseAsInputStream(url);
         try {
-            feed = (Feed) Abdera.getInstance().getParser().parse(inChannel,
-                    url.toString()).getRoot();
+            feed = (Feed) Abdera.getInstance().getParser().parse(
+                    inStream, url.toString()).getRoot();
 
             if (processFeedPage(url, feed)) {
                 IRI followingHref = feed.getLinkResolvedHref("prev-archive");
@@ -54,22 +108,22 @@ public abstract class FeedArchiveReader {
             throw new RuntimeException(e); /* TODO: stop on error?
             followingUrl = null; */
         } finally {
-            inChannel.close();
+            inStream.close();
         }
         return followingUrl;
     }
 
+    /**
+     * Template method intended for the feed processing.
+     * @return whether to continue backwards in time or stop.
+     */
+    public abstract boolean processFeedPage(URL pageUrl, Feed feed);
+
     public static String unescapeColon(String uriPath)
-        throws UnsupportedEncodingException
-    {
+        throws UnsupportedEncodingException {
         // FIXME: we have ":" url-escaped here (via Abdera resolved hrefs).
         // Is this a symptom of a brittle URI strategy in general?
         return uriPath.replace(URLEncoder.encode(":", "utf-8"), ":");
     }
-
-    /**
-     * @return whether to continue backwards in time or stop.
-     */
-    public abstract boolean processFeedPage(URL pageUrl, Feed feed);
 
 }
