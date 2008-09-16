1f6caf434cc69d2f1d3681c0d46fa3076b5da73b
nlm <nlm@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2008-09-16 08:51:19 +0000

    one proper feed reader test
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@120 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- packages/java/rinfo-base/src/test/groovy/se/lagrummet/rinfo/base/atom/FeedArchiveReaderTest.groovy
+++ packages/java/rinfo-base/src/test/groovy/se/lagrummet/rinfo/base/atom/FeedArchiveReaderTest.groovy
@@ -1,20 +1,46 @@
 package se.lagrummet.rinfo.base.atom
 
+import org.junit.AfterClass
+import org.junit.BeforeClass
 import org.junit.Test
+import static org.junit.Assert.*
 
 import org.apache.abdera.model.Feed
 import org.apache.abdera.model.Entry
 
+import org.restlet.Application
+import org.restlet.Component
+import org.restlet.Context
+import org.restlet.Directory
+import org.restlet.Restlet
+import org.restlet.data.Protocol
+
 
 class FeedArchiveReaderTest {
 
-    @Test(expected=MalformedURLException) // TODO: visit proper feed
+    private static component
+    private static testHttpPort = 9991
+
+    @BeforeClass
+    static void setupClass() {
+        component = new Component()
+        component.servers.add(Protocol.HTTP, testHttpPort)
+        component.clients.add(Protocol.FILE)
+        component.defaultHost.attach(new FileApp())
+        component.start()
+    }
+
+    @AfterClass
+    static void tearDownClass() {
+        component.stop()
+    }
+
+    @Test
     void shouldReadFeed() {
-        def testFeedUrlPath = null
+        def feedUrl =  new URL("http://localhost:${testHttpPort}/index.atom")
         def reader = new DummyFeeder()
-        reader.readFeed new URL(testFeedUrlPath)
-        assertEquals 1, reader.visitedPages
-        // TODO: assert.. visited?
+        reader.readFeed(feedUrl)
+        assertEquals 2, reader.visitedPages
     }
 
 }
@@ -25,7 +51,15 @@ class DummyFeeder extends FeedArchiveReader {
 
     boolean processFeedPage(URL pageUrl, Feed feed) {
         visitedPages += 1
-        return false
+        return true
     }
 
 }
+
+class FileApp extends Application {
+    static ROOT_URI = new File("src/test/resources/feed").toURI().toString()
+    Restlet createRoot() {
+        return new Directory(context, ROOT_URI)
+    }
+}
+
