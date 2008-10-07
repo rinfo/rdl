6194bf7e3463096e1d62b10c2d9aee136e8b212a
nlm <nlm@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2008-10-07 18:34:17 +0000

    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@159 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- packages/java/rinfo-main/src/scripts/serialize_collector_state.groovy
+++ packages/java/rinfo-main/src/scripts/serialize_collector_state.groovy
@@ -4,18 +4,24 @@ import org.openrdf.sail.nativerdf.NativeStore
 
 import se.lagrummet.rinfo.base.rdf.RDFUtil
 
+import static se.lagrummet.rinfo.collector.FeedCollectorStateLogger.COLLECTOR_NS
+
 if (args.length < 1) {
     println "Usage: <sesame-repo>"
     System.exit 1
 }
 def dataDir = new File(args[0])
 
-def statsRepo = new SailRepository(new NativeStore(dataDir))
-statsRepo.initialize()
+def repo = new SailRepository(new NativeStore(dataDir))
+repo.initialize()
+
+conn = repo.connection
+conn.setNamespace("rconn", COLLECTOR_NS)
+conn.close()
 
 def mtype = "application/x-turtle"
 //def mtype = "application/rdf+xml"
-RDFUtil.serialize(statsRepo, mtype, System.out)
+RDFUtil.serialize(repo, mtype, System.out)
 
-statsRepo.shutDown()
+repo.shutDown()
 
