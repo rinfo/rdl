8585e5702f8f34b7b6f9a1df853ebbfe52eed0e8
nlm <nlm@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2008-10-23 15:19:06 +0000

    added cmdline too for FileDepot (for manual (re-)indexing
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@185 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- packages/java/rinfo-store/README.txt
+++ packages/java/rinfo-store/README.txt
@@ -18,4 +18,5 @@ Running a class::
 
     $ mvn exec:java -Dexec.mainClass=se.lagrummet.rinfo.store.supply.SupplyApplication -Dexec.args="8182 [opt. path to depot-config-props]"
 
+    $ mvn exec:java -Dexec.mainClass=se.lagrummet.rinfo.store.depot.FileDepotCmdTool -Dexec.args="<path-to-depot.properties> index"
 
---
+++ packages/java/rinfo-store/src/main/java/se/lagrummet/rinfo/store/depot/FileDepotCmdTool.java
@@ -0,0 +1,24 @@
+package se.lagrummet.rinfo.store.depot;
+
+public class FileDepotCmdTool {
+
+    public static final String COMMANDS = "index";
+    // TODO:IMPROVE: more commands; e.g. check, ...
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.out.println(String.format(
+                    "Usage: s <confile-file> <%s>", COMMANDS));
+            System.exit(0);
+        }
+        FileDepot depot = FileDepot.newConfigured(args[0]);
+        String command = args[1];
+        if (command.equals("index")) {
+            depot.generateIndex();
+        } else {
+            System.out.println("Unknown command: " + command);
+            System.exit(1);
+        }
+    }
+
+}
