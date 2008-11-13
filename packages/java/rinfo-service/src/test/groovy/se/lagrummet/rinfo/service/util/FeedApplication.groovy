87d3630cb04a1935eb391174ee86c56f5c340f81
msher <msher@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2008-11-13 10:18:35 +0000

    Basic application for serving test feeds from file.
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@259 f32dfbcc-ef79-4080-89f5-1451246e9e1e

---
+++ packages/java/rinfo-service/src/test/groovy/se/lagrummet/rinfo/service/util/FeedApplication.groovy
@@ -0,0 +1,23 @@
+package se.lagrummet.rinfo.service.util
+
+import org.restlet.Application
+import org.restlet.Context
+import org.restlet.Restlet
+import org.restlet.Directory
+
+/**
+ * Basic application for serving feeds from file.
+ */
+public class FeedApplication extends Application {
+
+    static ROOT_URI = new File( "src/test/resources/feeds").toURI().toString()
+
+    public FeedApplication(Context parentContext) {
+        super(parentContext)
+    }
+
+    Restlet createRoot() {
+    	return new Directory(context, ROOT_URI)
+    }
+
+}
