a909be0dda64f0208488ef883fcfa4320631e232
msher <msher@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2008-11-14 08:52:17 +0000

    Tests for verifying ServiceApplication connection handling and feed ping requests.
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@263 f32dfbcc-ef79-4080-89f5-1451246e9e1e

---
+++ packages/java/rinfo-service/src/test/groovy/se/lagrummet/rinfo/service/ServiceApplicationConnectionTest.groovy
@@ -0,0 +1,149 @@
+package se.lagrummet.rinfo.service
+
+import org.junit.*
+import static org.junit.Assert.*
+
+import org.apache.commons.configuration.PropertiesConfiguration
+
+import org.restlet.Client
+import org.restlet.Component
+import org.restlet.VirtualHost
+import org.restlet.data.MediaType
+import org.restlet.data.Method
+import org.restlet.data.Protocol
+import org.restlet.data.Request
+import org.restlet.data.Response
+import org.restlet.data.Status
+
+import org.openrdf.repository.Repository
+import org.openrdf.repository.RepositoryConnection
+import org.openrdf.repository.event.RepositoryListener;
+import se.lagrummet.rinfo.service.util.FeedApplication
+import se.lagrummet.rinfo.service.util.RepoListener
+import se.lagrummet.rinfo.service.util.RepoConnectionListener
+
+/**
+ * Test for verifying ServiceApplication's handling of multiple connections.
+ * 
+ */
+class ServiceApplicationConnectionTest {
+
+    static final String CONFIG_PROPERTIES_FILE_NAME = "rinfo-service-test.properties"
+
+    static serviceAppUrl
+    static feedAppUrl
+    static sesameRepoPath
+    static repoListener
+    static component
+    
+    @BeforeClass
+    static void setupClass() {      
+        def config = new PropertiesConfiguration(CONFIG_PROPERTIES_FILE_NAME)
+        def serviceAppPort = config.getInt("rinfo.service.serviceAppPort")
+        def feedAppPort = config.getInt("rinfo.service.feedAppPort")
+        def appUrlBase = config.getString("rinfo.service.appUrlBase")
+        serviceAppUrl = appUrlBase + ":" + serviceAppPort
+        feedAppUrl = appUrlBase + ":" + feedAppPort
+        sesameRepoPath = config.getString("rinfo.service.sesameRepoPath")        
+
+        component = new Component()
+        def context = component.getContext()
+
+        // create a local ServiceApplication
+        def serviceApplication = new ServiceApplication(context.createChildContext())
+
+        // reconfigure ServiceApplication
+        serviceApplication.configure(config)
+        repoListener = new RListener()
+        serviceApplication.addRepositoryListener(repoListener)
+        serviceApplication.addRepositoryConnectionListener(repoListener)
+        
+        // create a local application that serves feeds
+        def feedApplication = new FeedApplication(context.createChildContext())     
+        
+        // start applications
+        def feedHost = new VirtualHost(context.createChildContext())
+        feedHost.setHostPort("" + feedAppPort)
+        feedHost.attach(feedApplication)
+
+        def serviceHost = new VirtualHost(context.createChildContext())
+        serviceHost.setHostPort("" + serviceAppPort)
+        serviceHost.attach(serviceApplication)
+        
+        component.with {
+            servers.add(Protocol.HTTP, serviceAppPort)
+            servers.add(Protocol.HTTP, feedAppPort)
+            clients.add(Protocol.HTTP)
+            clients.add(Protocol.FILE)            
+            hosts.add(serviceHost)            
+            hosts.add(feedHost)
+            start()
+        }       
+    }
+
+    @AfterClass
+    static void tearDownClass() {
+    	component.stop()
+    }
+
+    @Test
+    void testServiceApplicationConnection() {     
+        def request = new Request(Method.GET, serviceAppUrl + "/status")
+        def client = new Client(Protocol.HTTP)     
+        def response = client.handle(request)
+        assertEquals Status.SUCCESS_OK , response.status
+    }
+
+    @Test
+    void testFeedApplicationConnection() {     
+        def request = new Request(Method.GET, feedAppUrl + "/1-init.atom")
+        def client = new Client(Protocol.HTTP)     
+        def response = client.handle(request)
+        assertEquals Status.SUCCESS_OK , response.status        
+    }    
+    
+    @Test
+    void testConcurrentCalls() {
+    	repoListener.noofConnections = 0
+        repoListener.isConcurrent = false
+    	
+        def request = new Request(Method.POST, "${serviceAppUrl}")
+        def param = "feed=${feedAppUrl}/1-init.atom"        
+        request.setEntity(param, MediaType.MULTIPART_FORM_DATA)     
+        def client = new Client(Protocol.HTTP)     
+        def response = client.handle(request)
+        assertEquals Status.SUCCESS_OK , response.status                    
+
+        request = new Request(Method.POST, "${serviceAppUrl}")
+        request.setEntity(param, MediaType.MULTIPART_FORM_DATA)     
+        response = client.handle(request)
+        assertEquals Status.SUCCESS_OK , response.status        
+        
+        Thread.sleep(3000)
+
+        assertFalse "Concurrent connections to repository.", repoListener.isConcurrent        
+    }
+}
+
+/**
+ * Listener for checking if concurrent connections are opened to repository.
+ */
+class RListener extends RepoConnectionListener implements RepositoryListener {
+
+	 def noofConnections = 0
+	 def isConcurrent = false
+	
+	void initialize(Repository repo) { }
+    void setDataDir(Repository repo, File dataDir) { }
+    void shutDown(Repository repo) { }   
+
+    void getConnection(Repository repo, RepositoryConnection conn) {
+    	noofConnections++
+    	isConcurrent = isConcurrent | noofConnections > 1 
+    }
+
+	@Override
+    void close(RepositoryConnection conn) {
+		noofConnections--
+    }
+}
---
+++ packages/java/rinfo-service/src/test/groovy/se/lagrummet/rinfo/service/ServiceApplicationTest.groovy
@@ -0,0 +1,140 @@
+package se.lagrummet.rinfo.service
+
+import org.junit.*
+import static org.junit.Assert.*
+
+import org.apache.commons.configuration.PropertiesConfiguration
+
+import org.restlet.Client
+import org.restlet.Component
+import org.restlet.VirtualHost
+import org.restlet.data.MediaType
+import org.restlet.data.Method
+import org.restlet.data.Protocol
+import org.restlet.data.Request
+import org.restlet.data.Response
+import org.restlet.data.Status
+
+import org.openrdf.repository.Repository
+import org.openrdf.repository.sail.SailRepository
+import org.openrdf.sail.nativerdf.NativeStore
+
+import se.lagrummet.rinfo.service.util.FeedApplication
+
+/**
+ * Test for verifying that a ping to the ServiceApplication leads to reading 
+ * the feed and fetching its RDF metadata.
+ *  
+ * Cleans repository before and after testing.
+ */
+class ServiceApplicationTest {
+
+    static final String CONFIG_PROPERTIES_FILE_NAME = "rinfo-service-test.properties"
+
+    static serviceAppUrl
+    static serviceAppPort
+    static feedAppUrl
+    static feedAppPort    
+    static sesameRepoPath
+    static component
+    static config
+    
+    @BeforeClass
+    static void setupClass() {    	
+    	config = new PropertiesConfiguration(CONFIG_PROPERTIES_FILE_NAME)
+        serviceAppPort = config.getInt("rinfo.service.serviceAppPort")
+        feedAppPort = config.getInt("rinfo.service.feedAppPort")
+    	def appUrlBase = config.getString("rinfo.service.appUrlBase")
+        serviceAppUrl = appUrlBase + ":" + serviceAppPort
+        feedAppUrl = appUrlBase + ":" + feedAppPort
+        sesameRepoPath = config.getString("rinfo.service.sesameRepoPath")        
+        cleanRepository()
+    }
+
+    @AfterClass
+    static void tearDownClass() {        
+        cleanRepository()
+    }
+
+    @Before
+    void startComponent() {
+        component = new Component()
+        def context = component.getContext()
+
+        // create a local ServiceApplication
+        def serviceApplication = new ServiceApplication(context.createChildContext())
+
+        // reconfigure ServiceApplication
+        serviceApplication.configure(config)
+
+        // create a local application that serves feeds
+        def feedApplication = new FeedApplication(context.createChildContext())     
+        
+        // start applications
+        def feedHost = new VirtualHost(context.createChildContext())
+        feedHost.setHostPort("" + feedAppPort)
+        feedHost.attach(feedApplication)
+
+        def serviceHost = new VirtualHost(context.createChildContext())
+        serviceHost.setHostPort("" + serviceAppPort)
+        serviceHost.attach(serviceApplication)
+        
+        component.with {
+            servers.add(Protocol.HTTP, serviceAppPort)
+            servers.add(Protocol.HTTP, feedAppPort)
+            clients.add(Protocol.HTTP)
+            clients.add(Protocol.FILE)            
+            hosts.add(serviceHost)            
+            hosts.add(feedHost)
+            start()
+        }       
+    }
+
+    @After
+    void stopComponent() {
+    	component.stop()    	
+    }
+            
+    @Test
+    void testReadMetaFromFeed() {
+
+    	// add metadata
+        def request = new Request(Method.POST, "${serviceAppUrl}")
+        def param = "feed=${feedAppUrl}/1-init.atom"        
+        request.setEntity(param, MediaType.MULTIPART_FORM_DATA)    	
+        def client = new Client(Protocol.HTTP)     
+        def response = client.handle(request)
+        assertEquals Status.SUCCESS_OK , response.status            
+
+        Thread.sleep(3000)
+
+        stopComponent()
+
+        assertEquals 2, countContexts()
+    }
+
+    static void cleanRepository() {
+        def dataDir = new File(sesameRepoPath)
+        def sesameRepo = new SailRepository(new NativeStore(dataDir))
+        sesameRepo.initialize()        
+        def conn = sesameRepo.connection
+        conn.clear()
+        conn.clearNamespaces() 
+        conn.close()
+        sesameRepo.shutDown()       
+    }
+
+	static int countContexts() {
+        def dataDir = new File(sesameRepoPath)
+        def sesameRepo = new SailRepository(new NativeStore(dataDir))
+        sesameRepo.initialize()        
+        def conn = sesameRepo.connection
+        def res = conn.contextIDs
+        def i = res.asList().size()
+        res.close()
+        conn.close()
+        sesameRepo.shutDown()
+        return i
+	}
+
+}
