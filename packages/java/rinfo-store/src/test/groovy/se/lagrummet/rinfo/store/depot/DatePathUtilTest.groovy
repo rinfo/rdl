9647aebafec38141129e30597d3c3fc3256a1c05
nlm <nlm@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2008-11-21 14:43:58 +0000

    new (skeleton) test
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@285 f32dfbcc-ef79-4080-89f5-1451246e9e1e

---
+++ packages/java/rinfo-store/src/test/groovy/se/lagrummet/rinfo/store/depot/DatePathUtilTest.groovy
@@ -0,0 +1,29 @@
+package se.lagrummet.rinfo.store.depot
+
+import org.junit.Test
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import static org.junit.Assert.*
+
+class DatePathUtilTest {
+
+    @Test
+    void shouldMachValidDatePaths() {
+        /* TODO:
+        assertTrue match("09-30")
+        assertTrue match("19-30")
+        assertTrue match("29-30")
+        assertTrue match("12-30")
+        assertFalse match("13-30")
+        assertTrue match("12-31")
+        assertFalse match("12-32")
+        assertTrue match("12-01")
+        assertFalse match("12-00")
+
+        assertFalse match("T01_01_a1Z")
+        assertTrue match("T01_01_01Z")
+        assertTrue match("T13_28_37Z")
+        */
+    }
+
+}
