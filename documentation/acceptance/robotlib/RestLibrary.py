7b8b0d786ed4cd419a266d9e4073f5f40a9b9789
nlm <nlm@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2008-11-27 22:28:39 +0000

    adapted robotlib to use more of the stdlib (and refactored to support import-conditioned incremental mixin of features
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@313 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- documentation/acceptance/robotlib/RestLibrary.py
+++ documentation/acceptance/robotlib/RestLibrary.py
@@ -1,13 +1,64 @@
+# -*- coding: UTF-8 -*-
+"""
+This is a HTTP/REST client library, primarily designed for use as a
+`Robot Framework <http://robotframework.org/>`_ test library. It provides
+keywords for calling REST-style services and inspecting the response.
+"""
 import re
 from urlparse import urljoin
-import httplib2
-from lxml import etree
+from warnings import warn
+
+__author__ = "Niklas Lindström"
+__version__ = "1.0b"
+
+# Prefer httplib2, with fallback to (std) httplib.
+try:
+    from httplib2 import Http
+    def http_request(url, method, data=None, headers=None):
+        return Http().request(url, method, data, headers)
+except ImportError:
+    from urlparse import urlsplit
+    from httplib import HTTPConnection, HTTPSConnection
+    def http_request(url, method, data=None, headers=None):
+        scheme, netloc, path, query, fragment = urlsplit(url)
+        if scheme == 'https':
+            conn = HTTPSConnection(netloc)
+        else:
+            conn = HTTPConnection(netloc)
+        conn.request(method, "%s?%s" % (path, query), data, headers=headers)
+        response = conn.getresponse()
+        response.get = response.getheader # NOTE: monkey-patch
+        return response, response.read()
 
 
-class RestLibrary(object):
+# Generic assertion functions.
+
+def expect(key, expected, value):
+    assert expected == value, (
+            "Expected %s to be %r but was %r" % (key, expected, value))
+
+def expect_regexp(key, regexp, value):
+    assert re.match(regexp, value), (
+            "Expected %s to match regexp %r but was %r" % (key, regexp, value))
+
+def expect_exists(what, value):
+    assert value, "Expected %s to be present (got %r)." % (what, value)
+
+def expect_not_exists(what, value):
+    assert not value, "Expected no value for %s (got %r)." % (what, value)
+
+
+# Library implementation.
+# Use add_base to register a class as a base for the final library class.
+
+from collections import deque
+bases = deque()
+add_base = bases.appendleft
+
+
+class CoreRestClient(object):
 
     def __init__(self):
-        self._namespaces = {}
         self._status = None
         self._baseurl = None
         self._send_headers = {}
@@ -16,7 +67,20 @@ class RestLibrary(object):
     def _reset(self):
         self._response = None
         self._content = None
-        self._lazy_doc = None
+
+    def _do_request(self, method, url, data=None):
+        self._reset()
+        url = urljoin(self._baseurl, url)
+        response, content = http_request(url, method, data, headers=self._send_headers)
+        self._status = "%d %s" % (response.status, response.reason)
+        self._response = response
+        self._content = content
+
+    def _get_header(self, header):
+        return self._response.get(header.lower())
+
+
+class CoreRestLibrary(CoreRestClient):
 
     def base_url(self, url):
         self._baseurl = url
@@ -48,16 +112,21 @@ class RestLibrary(object):
     def patch(self, url, data):
         self._do_request("PATCH", url, data)
 
-    def follow(self):
-        c_loc = self._response.get('content-location')
-        expect_exists("response header 'Content-Location'", c_loc)
-        self.get(c_loc)
-
     def response(self, expected_status):
         expect("status", expected_status, self._status)
 
+    def follow(self):
+        header = 'Location'
+        c_loc = self._get_header(header)
+        # FIXME: should only use Location (and check for 30x), right?
+        if not c_loc:
+            header = 'Content-Location'
+            c_loc = self._get_header(header)
+        expect_exists("response header '%s'" % header, c_loc)
+        self.get(c_loc)
+
     def header(self, header, expected=None):
-        value = self._response.get(header.lower())
+        value = self._get_header(header)
         expect(header, expected, value)
 
     def body_is(self, expected):
@@ -68,6 +137,19 @@ class RestLibrary(object):
 
     def no_body(self):
         expect_not_exists("response body", self._content)
+
+add_base(CoreRestLibrary)
+
+
+class XmlMixinSupport(object):
+
+    def __init__(self):
+        super(XmlMixinSupport, self).__init__()
+        self._namespaces = {}
+
+    def _reset(self):
+        super(XmlMixinSupport, self)._reset()
+        self._lazy_doc = None
 
     def xmlns(self, pfx, uri):
         self._namespaces[pfx] = uri
@@ -85,43 +167,43 @@ class RestLibrary(object):
         return value
 
     def find_xpath(self, expr):
-        doc = self._get_parsed_doc()
-        value = doc.xpath(expr, namespaces=self._namespaces)
+        value = self._eval_xpath(expr)
         expect_exists("xpath %r" % expr, value)
         return value
 
     def no_xpath(self, expr):
-        doc = self._get_parsed_doc()
-        value = doc.xpath(expr, namespaces=self._namespaces)
+        value = self._eval_xpath(expr)
         expect_not_exists("xpath %r" % expr, value)
 
-    def _do_request(self, method, url, data=None):
-        self._reset()
-        h = httplib2.Http()
-        url = urljoin(self._baseurl, url)
-        response, content = h.request(url, method, data, headers=self._send_headers)
-        self._status = "%d %s" % (response.status, response.reason)
-        self._response = response
-        self._content = content
-
     def _get_parsed_doc(self):
-        if self._lazy_doc is None:
-            self._lazy_doc = etree.fromstring(self._content)
-        return self._lazy_doc
+        raise NotImplementedError("No XML parser available.")
+
+    def _eval_xpath(self, expr):
+        raise NotImplementedError("No XPath processor available.")
+
+try:
+    from lxml import etree
+
+    class XmlMixin(XmlMixinSupport):
+
+        def _get_parsed_doc(self):
+            if self._lazy_doc is None:
+                self._lazy_doc = etree.fromstring(self._content)
+            return self._lazy_doc
+
+        def _eval_xpath(self, expr):
+            doc = self._get_parsed_doc()
+            return doc.xpath(expr, namespaces=self._namespaces)
+
+    add_base(XmlMixin)
+
+except ImportError:
+    warn("Cannot parse XML responses. Missing module: lxml")
+    # TODO: Try some more options (amara, elementtree, javax.xml).
+    # If none can be found, disable XML keywords by removing the following line:
+    add_base(XmlMixinSupport)
 
 
-def expect(key, expected, value):
-    assert expected == value, (
-            "Expected %s to be %r but was %r" % (key, expected, value))
-
-def expect_regexp(key, regexp, value):
-    assert re.match(regexp, value), (
-            "Expected %s to match regexp %r but was %r" % (key, regexp, value))
-
-def expect_exists(what, value):
-    assert value, "Expected %s to be present (got %r)." % (what, value)
-
-def expect_not_exists(what, value):
-    assert not value, "Expected no value for %s (got %r)." % (what, value)
+RestLibrary = type('RestLibrary', tuple(bases), {})
 
 
