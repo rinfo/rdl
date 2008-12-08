cc83c5c31f45225971ea686e8140ed0fde51a52e
msher <msher@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2008-12-08 12:15:45 +0000

    Moving environment specific configuration files.
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@356 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- packages/java/rinfo-sesame-http/conf/tomcat/setenv.sh
+++
@@ -1,2 +0,0 @@
-export CATALINA_PID=/usr/share/tomcat6/bin/tomcat.pid
-export JAVA_OPTS="-Dinfo.aduna.platform.appdata.basedir=/opt/_workapps/rinfo/aduna -Xms512M -Xmx1024M -Duser.language=sv -Duser.country=SE"
