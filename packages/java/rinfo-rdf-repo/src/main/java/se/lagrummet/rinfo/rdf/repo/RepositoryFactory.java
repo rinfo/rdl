3f66b7351272b1e83e52f060854a99993c8f7fad
msher <msher@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2008-12-11 13:06:03 +0000

    Extend RepositoryFactory with native backend.
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@362 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- packages/java/rinfo-rdf-repo/src/main/java/se/lagrummet/rinfo/rdf/repo/RepositoryFactory.java
+++ packages/java/rinfo-rdf-repo/src/main/java/se/lagrummet/rinfo/rdf/repo/RepositoryFactory.java
@@ -1,15 +1,19 @@
 package se.lagrummet.rinfo.rdf.repo;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.lang.StringUtils;
 import org.openrdf.repository.Repository;
+import org.openrdf.sail.Sail;
 import org.openrdf.sail.inferencer.fc.DirectTypeHierarchyInferencer;
 import org.openrdf.sail.inferencer.fc.ForwardChainingRDFSInferencer;
 import org.openrdf.sail.memory.MemoryStore;
+import org.openrdf.sail.nativerdf.NativeStore;
 import org.openrdf.repository.sail.SailRepository;
 
 public class RepositoryFactory {
@@ -17,7 +21,8 @@ public class RepositoryFactory {
     private static final String PROPERTIES_FILE_NAME = "rinfo-rdf-repo.properties";
 
     private static final List<String> SUPPORTED_TRIPLE_STORES = Arrays.asList(
-			"sesame", "jena", "mulgara", "swiftowlim", "openlink-virtuoso");
+			"sesame");
+//    "sesame", "jena", "mulgara", "swiftowlim", "openlink-virtuoso");
 
 	private static Repository repository;
 
@@ -43,24 +48,34 @@ public class RepositoryFactory {
 	}
 	
 	private static void init(Configuration config) throws Exception {
+
 		validateConfiguration(config);
 
 		String store = config.getString("triple.store").toLowerCase();
 		String backend = config.getString("backend").toLowerCase();
+		String dataDir = config.getString("data.dir");
+		String repoName = config.getString("repository.name");
 		
 		if (store.equals("sesame")) {
 
 			boolean inference = config.getBoolean("inference");
 			boolean inferenceDT = config.getBoolean("inference.direct.type");
-
-			if (inference) {
+			
+			Sail sail = null;			
+			if (backend.equals("memory")) {
+				sail = new MemoryStore();
+			} else if (backend.equals("native")) {
+				sail = new NativeStore(new File(dataDir + "/" + repoName));				
+			}
+			
+			if (inference) {				
 				repository = new SailRepository(
-						new ForwardChainingRDFSInferencer(new MemoryStore()));								
+						new ForwardChainingRDFSInferencer(sail));								
 			} else if (inferenceDT) {
 				repository = new SailRepository(
-						new DirectTypeHierarchyInferencer(new MemoryStore()));								
+						new DirectTypeHierarchyInferencer(sail));								
 			} else {
-				repository = new SailRepository(new MemoryStore());				
+				repository = new SailRepository(sail);				
 			}
 
 			repository.initialize();
@@ -70,16 +85,31 @@ public class RepositoryFactory {
 	
 	private static void validateConfiguration(Configuration config) throws Exception {		
 		String store = config.getString("triple.store").toLowerCase();
+		String backend = config.getString("backend").toLowerCase();
+		String dataDir = config.getString("data.dir");
+		String repoName = config.getString("repository.name");
 		boolean inference = config.getBoolean("inference");
 		boolean inferenceDT = config.getBoolean("inference.direct.type");
 		
+		if (StringUtils.isEmpty(store)) {
+			throw new Exception("Missing property 'store'.");						
+		}		
+		if (StringUtils.isEmpty(backend)) {
+			throw new Exception("Missing property 'backend'.");						
+		}
+		if (backend.equals("native") && StringUtils.isEmpty(dataDir)) {
+			throw new Exception("Missing property 'data.dir'.");									
+		}
+		if (backend.equals("native") && StringUtils.isEmpty(repoName)) {
+			throw new Exception("Missing property 'repository.name'.");									
+		}		
 		if (!SUPPORTED_TRIPLE_STORES.contains(store)) {
 			throw new Exception("Unsupported triple store: " + store);			
-		}
-		
+		}		
 		if (inference && inferenceDT) {
 			throw new Exception("Conflicting properties. 'inference' and " 
 					+ "'inference.direct.type' cannot both be true");				
-		}		
+		}
+
 	}
 }
