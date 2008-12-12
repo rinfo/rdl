1bd941e95e27bd119903a7855fedb2883694fdcb
msher <msher@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2008-12-12 12:10:16 +0000

    Tests of local and remote Sesame repository.
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@371 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- packages/java/rinfo-rdf-repo/src/test/java/se/lagrummet/rinfo/rdf/repo/TestRepositoryUtil.java
+++ packages/java/rinfo-rdf-repo/src/test/java/se/lagrummet/rinfo/rdf/repo/TestRepositoryUtil.java
@@ -15,36 +15,82 @@ public class TestRepositoryUtil extends TestCase {
 		
     private static final String PROPERTIES_FILE_NAME = "rinfo-rdf-repo.properties";
 
-    @SuppressWarnings({ "static-access" })
-	private void testAddDelete(Configuration config) throws Exception {
-    	RepositoryFactory factory = new RepositoryFactory(config); 
-		Repository repo = factory.getRepository();
-		RepositoryConnection conn = repo.getConnection();
-		assertTrue("Expected repository to be empty.", conn.isEmpty());
-		String s = "http://example.org/s";
-		String p = "http://example.org/p";
-		String o = "http://example.org/o";		
-		Statement st = new StatementImpl(new URIImpl(s), new URIImpl(p), new URIImpl(o));
-		conn.add(st);
-		assertEquals(1, conn.size());
-		RepositoryUtil.cleanRepository();
-		assertTrue("Expected repository to be empty.", conn.isEmpty());
-		assertEquals(0, conn.getContextIDs().asList().size());		
-		assertEquals(0, conn.getNamespaces().asList().size());    	
-    }
+    /*
+     * TODO: add tests for verifying behaviour of using inference / DT.
+     */
     
-	public void testSesameMemory() throws Exception {		
-		Configuration config = new PropertiesConfiguration(PROPERTIES_FILE_NAME);
-		config.setProperty("repository.name", "rinfo-test");
-		config.setProperty("backend", "memory");
-		testAddDelete(config);
+	public void testLocalSesameMemory() throws Exception {
+		testAddDelete("sesame", "memory", true);
 	}
-	
-	public void testSesameNative() throws Exception {
+
+	public void testLocalSesameNative() throws Exception {
+		testAddDelete("sesame", "native", true);
+	}
+
+	public void testRemoteSesameMemory() throws Exception {
+		testAddDelete("sesame", "memory", false);
+	}
+
+	public void testRemoteSesameNative() throws Exception {
+		testAddDelete("sesame", "native", false);
+	}
+
+	@SuppressWarnings("static-access")
+	private void testAddDelete(String tripleStore, String backend, boolean isLocalRepository) 
+	throws Exception {
+		Configuration config = getConfiguration(tripleStore, backend);
+		RepositoryFactory factory = new RepositoryFactory(config);
+		RepositoryConnection conn = null;
+		try {
+			Repository repo;
+			if (isLocalRepository) {
+				repo = factory.getLocalRepository();
+			} else {
+				repo = factory.getRemoteRepository();
+			}
+			conn = repo.getConnection();			
+			assertTrue("Expected repository to be empty.", conn.isEmpty());
+			String s = "http://example.org/s";
+			String p = "http://example.org/p";
+			String o = "http://example.org/o";		
+			Statement st = new StatementImpl(new URIImpl(s), new URIImpl(p), new URIImpl(o));
+			conn.add(st);			
+			assertEquals(1, conn.size());
+			conn.close();
+			
+			if (isLocalRepository) {
+				RepositoryUtil.cleanLocalRepository(config);				
+			} else {
+				RepositoryUtil.cleanRemoteRepository(config);								
+			}
+			
+			if (isLocalRepository) {
+				repo = factory.getLocalRepository();
+			} else {
+				repo = factory.getRemoteRepository();
+			}
+			conn = repo.getConnection();
+			assertTrue("Expected repository to be empty.", conn.isEmpty());
+			assertEquals(0, conn.getContextIDs().asList().size());		
+			assertEquals(0, conn.getNamespaces().asList().size());    				
+		} finally {
+			if (conn != null) {
+				conn.close();
+			}
+			if (factory != null) {
+				factory.shutDown();
+			}
+		}
+    }
+
+	private Configuration getConfiguration(String tripleStore, String backend) 
+	throws Exception  {
 		Configuration config = new PropertiesConfiguration(PROPERTIES_FILE_NAME);
-		config.setProperty("repository.name", "rinfo-test");
-		config.setProperty("backend", "native");
-		testAddDelete(config);		
+		String testRepoId = config.getString("test.repository.id");
+		config.setProperty("repository.id", testRepoId);
+		config.setProperty("triple.store", tripleStore);
+		config.setProperty("backend", backend);
+    	return config;
 	}
 
 }
