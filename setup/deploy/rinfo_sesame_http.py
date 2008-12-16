793b3035752c0aa0a6199955c6c979acd7de8d55
msher <msher@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2008-12-16 13:50:00 +0000

    Updating scripts for targets 'clean_repo' and 'setup_repo'.
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@382 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- setup/deploy/rinfo_sesame_http.py
+++ setup/deploy/rinfo_sesame_http.py
@@ -5,11 +5,13 @@ _needs_sesame_http_env = requires(
         'env', 'host_map', 'dist_dir', 'rinfo_dir', 'rinfo_rdf_repo_dir', 'tomcat',
         provided_by=[dev_unix, staging, production])
 
+
 @_needs_sesame_http_env
 def setup_sesame_http():
     run("mkdir $(dist_dir)", fail='ignore')
     sudo("mkdir $(rinfo_dir)", fail='ignore')
     sudo("mkdir $(rinfo_rdf_repo_dir)", fail='ignore')
+
 
 @depends(setup_sesame_http)
 def prepare_deploy_sesame_http():
@@ -28,6 +30,7 @@ def prepare_deploy_sesame_http():
         tomcat_conf_dir=conf_dir,
     )
 
+
 @depends(prepare_deploy_sesame_http)
 def deploy_sesame_http():
     dest_war = "$(dist_dir)/$(sesame_http_war_name).war"
@@ -38,18 +41,13 @@ def deploy_sesame_http():
     sudo("mv $(dist_dir)/$(sesame_http_war_name).war $(tomcat_webapps)/$(sesame_http_war_name).war")
     sudo("$(tomcat_start)")
 
+
 ##
 # Clean repository
 
-def clean_repo():
-    local("rm -rf /opt/_workapps/rinfo/aduna", fail='warn')
-    local("mkdir /opt/_workapps/rinfo/aduna", fail='warn')
-
-##
-# Setup empty repository
-
-@requires('env', provided_by=[dev_unix, staging, production])
-def setup_sesame_http_repo():
+# TODO (performance): reuse existing assembly zip instead of rebuilding each time.
+@depends(setup_sesame_http)
+def prepare_handle_sesame_repo():
     pkg_dir = "../packages/java/rinfo-rdf-repo"
     local("cd %s; mvn -P $(env) assembly:assembly" % pkg_dir)
     import ConfigParser    
@@ -58,11 +56,26 @@ def setup_sesame_http_repo():
     proj_name = cf.get("main", "rinfo.rdf.repo.version")
     zip_file = "%s/target/%s-with-dependencies.zip" % (pkg_dir, proj_name)
     remote_file_name = "%s-$(fab_timestamp)" % proj_name
-    run("mkdir $(dist_dir)", fail='ignore')        
     put(zip_file, "$(dist_dir)/%s.zip" % remote_file_name)
     run("cd $(dist_dir); unzip %s.zip" % remote_file_name)
     run("cd $(dist_dir); mv %s %s" % (proj_name, remote_file_name))
-    run("cd $(dist_dir)/%s; java -jar %s.jar setup remote" % (remote_file_name, proj_name))
+    config(
+        rinfo_rdf_repo_dir="$(dist_dir)/%s" % remote_file_name,
+        rinfo_rdf_repo_version=proj_name,
+    )
+
+@depends(prepare_handle_sesame_repo)
+def clean_repo():
+    run("cd $(rinfo_rdf_repo_dir); java -jar $(rinfo_rdf_repo_version).jar clean remote")
+
+
+##
+# Setup repository
+
+@depends(prepare_handle_sesame_repo)
+def setup_repo():
+    run("cd $(rinfo_rdf_repo_dir); java -jar $(rinfo_rdf_repo_version).jar setup remote")
+
 
 ##
 # Tomcat configuration
