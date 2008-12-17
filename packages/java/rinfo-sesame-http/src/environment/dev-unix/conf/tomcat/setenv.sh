aa2765a66a4ea22f1ed6e3feef2226fba4b6bfa3
msher <msher@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2008-12-17 13:20:37 +0000

    Reorganising environment/dev-unix/conf.
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@385 f32dfbcc-ef79-4080-89f5-1451246e9e1e

---
+++ packages/java/rinfo-sesame-http/src/environment/dev-unix/conf/tomcat/setenv.sh
@@ -0,0 +1,2 @@
+export CATALINA_PID=/usr/share/tomcat6/bin/tomcat.pid
+export JAVA_OPTS="-Dinfo.aduna.platform.appdata.basedir=/opt/_workapps/rinfo/aduna -Xms512M -Xmx1024M -Duser.language=sv -Duser.country=SE"
