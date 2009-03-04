1611327af9145acc90cc74937d234e53506221a7
nlm <nlm@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2009-03-04 10:57:07 +0000

    changed test script to run against locally loaded n3 files
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@477 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- packages/java/rinfo-base/src/scripts/sparqltree_test_runner.groovy
+++ packages/java/rinfo-base/src/scripts/sparqltree_test_runner.groovy
@@ -4,22 +4,31 @@ import javax.xml.transform.stream.StreamSource
 
 import se.lagrummet.rinfo.base.rdf.SparqlTree
 
-//import se.lagrummet.rinfo.base.rdf.RDFUtil
-//def repo = RDFUtil.createMemoryRepository()
-import org.openrdf.repository.sail.SailRepository
-import org.openrdf.sail.nativerdf.NativeStore
-def repo = new SailRepository(new NativeStore(new File("/Users/nlm/Library/Application Support/Aduna/OpenRDF Sesame/repositories/rinfo/")))
-repo.initialize()
+import se.lagrummet.rinfo.base.rdf.RDFUtil
+import static org.apache.commons.io.FileUtils.iterateFiles
+
+
+def repo = RDFUtil.createMemoryRepository()
+def addN3 = {
+    RDFUtil.loadDataFromFile(repo, it)
+}
+
+def base = "../../../resources/base/"
+iterateFiles(new File(base, "model"),
+        ["n3"] as String[], true).each {
+    addN3 it
+}
+
+iterateFiles(new File(base, "extended/rdf"),
+    ["n3"] as String[], true).each {
+    addN3 it
+}
 
 
 def treeDir = new File("../../../laboratory/services/SparqlToAtom/examples/")
+def rqTree = new SparqlTree(repo, new File(treeDir, "model/sparqltree-model.xml"))
 
-def rqTree = new SparqlTree(repo, new File(treeDir, "rdata/rpubl-rqtree.xml"))
-
-//def doc = rqTree.queryToTreeDocument()
-//SparqlTree.TRANSFORMER_FACTORY.newTransformer().transform(
-//    new DOMSource(doc), new StreamResult(System.out))
 def outputXslt = SparqlTree.TRANSFORMER_FACTORY.newTemplates(
-        new StreamSource(new File(treeDir, "rdata/rpubl_to_rdata.xslt")))
+        new StreamSource(new File(treeDir, "model/modeltree_to_html.xslt")))
 rqTree.queryAndChainToResult(new StreamResult(System.out), outputXslt)
 
