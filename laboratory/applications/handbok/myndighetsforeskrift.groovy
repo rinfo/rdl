f96301090293dc790d33dd132584db114c9e40a4
pkr <pkr@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2009-03-13 14:52:58 +0000

    Flying saucer w external source files working.
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@523 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- laboratory/applications/handbok/myndighetsforeskrift.groovy
+++ laboratory/applications/handbok/myndighetsforeskrift.groovy
@@ -1,26 +1,44 @@
-
 import org.xhtmlrenderer.pdf.ITextRenderer
 import com.lowagie.text.DocumentException
 import javax.xml.parsers.DocumentBuilder
 import javax.xml.parsers.DocumentBuilderFactory
-import java.io.FileOutputStream
-import java.io.File
+import java.io.*
+import org.w3c.dom.Document
+import com.uwyn.jhighlight.renderer.XhtmlRendererFactory
+import com.uwyn.jhighlight.tools.FileUtils
 
+input_file = "exempel.xhtml"
+pdf_filename = "exempel.pdf"
+infile = new File(input_file) 
+doc_xml = infile.getText("UTF-8")
 
-pdf_filename = "Handbok-Myndighetsforeskrifter.pdf"
-input_file = "myndighetsforeskrift.xhtml"
-//doc_xml = new File(input_file).getText("UTF-8")
+//Find div.sourcecode sections and load references highlighted source files
+(doc_xml =~ /<div class=\"sourcecode\">(.*)<\\/div>/).each {match -> 
 
-url = new File(input_file).toURI().toURL().toString()
+    source= new File(match[1]).getText("UTF-8")
 
-builder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-doc = builder.parse(url)
+    hl_renderer = XhtmlRendererFactory.getRenderer(FileUtils.getExtension("test.xml"))
+    hl_source_fragment = hl_renderer.highlight("", source, "iso-8859-1", true)
+
+    //Replace sourcecode section with highlighted source
+    doc_xml = doc_xml.replace("<div class=\"sourcecode\">" + match[1] + "</div>", "<div class=\"sourcecode\">" + hl_source_fragment + "</div>")
+}
+
+builder     = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+inputStream = new ByteArrayInputStream(doc_xml.getBytes("UTF-8"))
+org.w3c.dom.Document doc = builder.parse(inputStream)
+inputStream.close()
 
 renderer = new ITextRenderer()
 
-//Load fonts
+//Add font variants for embedding
 renderer.getFontResolver().addFont("gara.ttf", true);
+renderer.getFontResolver().addFont("garait.ttf", true);
+renderer.getFontResolver().addFont("garabd.ttf", true);
 renderer.getFontResolver().addFont("trebuc.ttf", true);
+renderer.getFontResolver().addFont("trebucit.ttf", true);
+renderer.getFontResolver().addFont("trebucbi.ttf", true);
+renderer.getFontResolver().addFont("trebucbd.ttf", true);
 
 renderer.setDocument(doc, null)
 renderer.layout()
