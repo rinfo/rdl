0eb1d11f8ebc5570c9aec5d44d879c3a2cea3626
nlm <nlm@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2009-04-07 14:54:03 +0000

    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@573 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- tools/setclasspath.sh
+++ tools/setclasspath.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-OUTDIR=$(pwd)
-if [ "$1" != "" ]; then
-    pushd $1
-    mvn dependency:build-classpath -Dmdep.outputFile=$OUTDIR/classpath.txt
+generate_classpath_file() {
+    outfile=$1/classpath.txt
+    pkgdir=$2
+    pushd $pkgdir
+        mvn dependency:build-classpath -Dmdep.outputFile=$outfile
     popd
-fi
+    echo -n ":$pkgdir/target/classes/" >> $outfile
+    echo -n ":$pkgdir/src/main/groovy/" >> $outfile
+}
 
-MVN_DEPS=$(cat classpath.txt)
-export CLASSPATH=$MVN_DEPS
+if [ "$1" != "" ]; then
+    generate_classpath_file $(pwd) $1
+fi
+export CLASSPATH=$(cat classpath.txt)
 
