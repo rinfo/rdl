d3e052078bf272200d1596e4bb9a6a11e063328c
pkr <pkr@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2009-04-24 14:38:27 +0000

    Dokumentation + deleted entry v1
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@610 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- laboratory/applications/lagrumsapp/README.txt
+++ laboratory/applications/lagrumsapp/README.txt
@@ -1,22 +1,38 @@
+Om exempelapplikationen
+-----------------------
+
+Syftet med denna exempelapplikation är att illustrera i programkod hur man kan
+implementera de format som rekommenderas av Rättsinformationsprojektet för den
+mest grundläggande nivån. Tanken är inte att tillhandahålla ett färdigt system
+för rättsinformationshantering.
+
+Applikationem är byggd på webbramverket Django som ges ut under BSD-licensen.
+Se BSD-licensen för mer information om möjligheter att använda dig av
+programkoden. Domstolsverket ger inga garantier för dess funktion eller
+lämplighet och frånsäger sig ansvar för eventuella fel och brister.
+
+Vi är dock tacksamma för feedback och rapporter om eventuella fel.
 
 Installationsansvisningar
 -------------------------
 
-Rader som börjar med "$" avser kommandon som skall utföras från ett terminalfönster).
+Rader som börjar med "$" avser kommandon som skall utföras från ett
+terminalfönster).
 
-1. Installera programspråket Python
+1. Installera programspråket Python 2.6 (Se http://www.python.org/download/).
 
-2. Installera easy_install
+2. Installera easy_install/setuptools (Se
+http://pypi.python.org/pypi/setuptools och
+http://peak.telecommunity.com/DevCenter/EasyInstall)
 
 3. Installera Django
+    $ easy_install django
 
-4. Installera Sqllite 3
-
-5. Installera applikationen
+4. Installera Sqllite 3 (Se http://www.sqlite.org/)
 
 6. Redigera settings.py och ange:
-    1. DATABASE_NAME    Sökväg till databasfilen
-    2. RINFO_ORG_URI    Identifieraren (i URI-format) för din organisation (erhålls från projektet)
+    DATABASE_NAME    Sökväg till databasfilen
+    Modifiera vid behov de inställningar som börjar med RINFO
 
 7. Installera databasschemat
     $ python manage.py syncdb
@@ -24,4 +40,10 @@ Rader som börjar med "$" avser kommandon som skall utföras från ett terminalföns
 8. Starta den inbyggda webbservern
     $ python manage.py runserver
 
-9. Öppna webbläsaren med följande adress: http://127.0.0.1:8000/admin för att redigera innehåll. Logga in som admin/admin.
+9. Öppna webbläsaren med följande adress: http://127.0.0.1:8000/
+    Exempelwebbplatsen visas. För att redigera innehåll navigera till
+http://127.0.0.1/admin/ och logga in som användare "admin" med lösenord
+"admin".
+
+
+
--- laboratory/applications/lagrumsapp/rinfo/models.py
+++ laboratory/applications/lagrumsapp/rinfo/models.py
@@ -11,90 +11,130 @@ from django.utils.feedgenerator import rfc3339_date
 
 
 class Forfattningssamling(models.Model):
-    # Namn på författningssamling
-    titel = models.CharField(max_length=255, unique=True, help_text="Namn på författningssamling, t.ex. <em>Exempelmyndighetens författningssamling'</em>")
+    """Modell för författningssamlingar."""
 
-    # Kortnamn på författningssamling, t.ex. "RA-FS"
-    kortnamn = models.CharField(max_length=10, unique=True, help_text="T.ex. <em>EXFS</em>")
+    # Namn på författningssamling
+    titel=models.CharField(
+            max_length=255, 
+            unique=True, 
+            help_text="""Namn på författningssamling, t.ex.
+            <em>Exempelmyndighetens författningssamling</em>""")
+
+    # Kortnamn på författningssamling, t.ex. "EXFS"
+    kortnamn=models.CharField(
+                max_length=10, 
+                unique=True, 
+                help_text="""T.ex. <em>EXFS</em>""")
 
     # Författningssamlingens unika identifierare, t.ex.
     # "http://rinfo.lagrummet.se/serie/fs/ra-fs". Denna erhålls från
     # projektet.
-    identifierare = models.URLField(verify_exists=False, max_length=255, unique=True, help_text="Erhålls från Domstolsverket")
+    identifierare=models.URLField(
+            verify_exists=False, 
+            max_length=255,
+            unique=True, 
+            help_text="Erhålls från Domstolsverket")
 
     def __unicode__(self):
         return u'%s %s' % (self.titel, self.kortnamn)
 
+    # Inställningar för etiketter i administrationsgränssnittet.
     class Meta:
-        verbose_name = u"Författningssamling"
-        verbose_name_plural = u"Författningssamlingar"
+        verbose_name=u"Författnings-samling"
+        verbose_name_plural=u"Författnings-samlingar"
 
 
 
 class Amnesord(models.Model):
+    """Modell för ämnesord."""
+
     # Namn, t.ex. "Arkivgallring"
-    titel = models.CharField(max_length=255, unique=True, help_text="Ämnesordets titel")
+    titel=models.CharField(
+            max_length=255, 
+            unique=True, 
+            help_text="Ämnesordet")
 
     # Beskrivning/definition av ämnesordet
-    beskrivning = models.TextField(blank=True)
+    beskrivning=models.TextField(blank=True)
 
     def __unicode__(self):
         return self.titel
 
+    # Inställningar för etiketter i administrationsgränssnittet.
     class Meta:
-        verbose_name = u"Ämnesord"
-        verbose_name_plural = u"Ämnesord"
+        verbose_name=u"Ämnesord"
+        verbose_name_plural=u"Ämnesord"
 
 
     
 
 class Bemyndigandeparagraf(models.Model):
+    """Modell för att hantera bemyndigandeparagrafer."""
+
     # Namn, t.ex. "Arkivförordningen"
-    titel = models.CharField(max_length=255, help_text="T.ex. <em>Arkivförordningen</em>")
+    titel=models.CharField(max_length=255,
+                             help_text="""T.ex. <em>Arkivförordningen</em>""")
 
     # SFS-nummer, t.ex. "1991:446"
-    sfsnummer = models.CharField("SFS-nummer", max_length=10, blank=False, help_text="T.ex. <em>1991:446</em>")
+    sfsnummer=models.CharField("SFS-nummer", max_length=10, blank=False,
+            help_text="T.ex. <em>1991:446</em>")
 
     # Paragrafnummer, t.ex. "11"
-    paragrafnummer = models.CharField(max_length=10, blank=True, help_text="T.ex. <em>12</em>")
+    paragrafnummer=models.CharField(max_length=10, blank=True,
+            help_text="T.ex. <em>12</em>")
     
     def __unicode__(self):
         return u"%s (%s) %s" % (self.titel, self.sfsnummer, self.paragrafnummer)
 
+    # Inställningar för etiketter i administrationsgränssnittet.
     class Meta:
-        verbose_name = u"Bemyndigandeparagraf"
-        verbose_name_plural = u"Bemyndigandeparagrafer"
+        verbose_name=u"Bemyndigandeparagraf"
+        verbose_name_plural=u"Bemyndigandeparagrafer"
 
 
 
 class Myndighetsforeskrift(models.Model):
-    # Foreskriftens titel
-    titel = models.CharField(max_length=512, unique=True)
+    """Modell för myndighetsföreskrifter. Denna hanterar det huvudsakliga
+    innehållet i en författningssamling - själva föreskrifterna. Den kan enkelt
+    utökas med fler egenskaper."""
+
+    # Föreskriftens officiella titel
+    titel=models.CharField(
+            max_length=512, 
+            unique=True, 
+            help_text="""T.ex. <em>Exempelmyndighetens föreskrifter och
+            allmänna råd om arkiv hos statliga myndigheter;</em>""")
 
     # Författningssamlingsnummer, t.ex. "2006:6"
-    fsnummer = models.CharField("Författningssamlingsnummer", max_length=10, unique=True, blank=False, help_text="T.ex. <em>2006:6</em>")
+    fsnummer=models.CharField("FS-nummer", max_length=10,
+            unique=True, blank=False, help_text="T.ex. <em>2006:6</em>")
 
     # Utfärdandedatum, t.ex. 2007-02-09 
-    utfardandedag = models.DateField("Utfärdandedag", blank=False)
+    utfardandedag=models.DateField("Utfärdandedag", blank=False)
 
     # Ikraftträdandedatum, t.ex. 2007-02-01
-    ikrafttradandedag = models.DateField("Ikraftträdandedag", blank=False)
+    ikrafttradandedag=models.DateField("Ikraftträdande-dag", blank=False)
 
     # Utkom från tryck datum, t.ex. 2007-02-09
-    utkom_fran_tryck = models.DateField("Utkom från tryck", blank=False)
+    utkom_fran_tryck=models.DateField("Utkom från tryck", blank=False)
 
     # Författningssamling (referens till post i de upprättade
     # författningssamlingarna)
-    forfattningssamling = models.ForeignKey(Forfattningssamling, blank=False, verbose_name=u"författningssamling")
+    forfattningssamling=models.ForeignKey(Forfattningssamling, blank=False,
+            verbose_name=u"författningssamling")
 
     # Bemyndiganden (referenser till bemyndigandeparagrafer)
-    bemyndigandeparagrafer = models.ManyToManyField(Bemyndigandeparagraf, blank=False, verbose_name=u"referenser till bemyndiganden")
+    bemyndigandeparagrafer=models.ManyToManyField(Bemyndigandeparagraf,
+            blank=False, verbose_name=u"referenser till bemyndiganden")
 
     # PDF-version av dokumentet
-    dokument = models.FileField(u"PDF-version av föreskrift", upload_to="dokument", blank=False, help_text=u"Se till att dokumentet är i PDF/A format.")
+    dokument=models.FileField(u"PDF-version av föreskrift",
+            upload_to="dokument", 
+            blank=False, 
+            help_text="""Se till att dokumentet är i PDF/A format.""")
 
     # Koppling till ämnesord
-    amnesord = models.ManyToManyField(Amnesord, blank=True, verbose_name=u"ämnesord")
+    amnesord=models.ManyToManyField(Amnesord, blank=True, verbose_name=u"ämnesord")
 
     # Returnera bara årtalet från ikraftträdandedagen
     def ikrafttradandear(self):
@@ -103,7 +143,8 @@ class Myndighetsforeskrift(models.Model):
     # Metod för att erhålla URL:en till en eneksild föreskrift
     @models.permalink
     def get_absolute_url(self): 
-        return ('lagrumsapp.rinfo.views.foreskrift', [str(self.forfattningssamling.kortnamn), str(self.fsnummer)])
+        return ('lagrumsapp.rinfo.views.foreskrift',
+                [str(self.forfattningssamling.kortnamn), str(self.fsnummer)])
 
     # Metod för att skapa rättsinformationssystemets unika identifierare för denna post
     def get_rinfo_uri(self):
@@ -114,85 +155,111 @@ class Myndighetsforeskrift(models.Model):
     def __unicode__(self):
         return u'%s %s' % (self.fsnummer, self.titel)
 
-    # Metod för att skapa den standardiserade metadataposten om denna föreskrift
     def to_rdfxml(self):
-        template = loader.get_template('foreskrift_rdf.xml')
-        context = Context({ 'foreskrift': self, 'publisher_uri': settings.RINFO_ORG_URI, 'rinfo_base_uri': settings.RINFO_BASE_URI})
+        """Metod för att skapa den standardiserade metadataposten om denna
+        föreskrift."""
+
+        template=loader.get_template('foreskrift_rdf.xml')
+        context=Context({ 'foreskrift': self, 'publisher_uri':
+            settings.RINFO_ORG_URI, 'rinfo_base_uri': settings.RINFO_BASE_URI})
         return template.render(context)
 
-    # Override av save-metoden. I samband med att en föreskrift
-    # sparas/uppdateras skriver vi även ner ett nytt Atom entry så att Atom-feeden
-    # är uppdaterad.
-    def save(self, *args, **kw):
-        published = datetime.now()
 
-        # Kolla om detta är en uppdatering eller ett nytt dokument
+    def save(self, *args, **kw):
+        """Override av save-metoden. I samband med att en föreskrift
+        sparas/uppdateras skriver vi även ner ett nytt Atom entry så att
+        Atom-feeden är uppdaterad."""
+
+        # Då posten publicerades (nu, om det är en ny post)
+        published=datetime.now()
+
+        # Kolla om detta är en uppdatering eller ett nytt dokument.
         if self.id:
-            # Uppdatering - hitta publiceringsdatumet från tidigare entry-post
+            # Uppdatering - hitta publiceringsdatumet från tidigare post.
             try:
-                foreskrift_entries = AtomEntry.objects.filter(myndighetsforeskrift=self).order_by("published")
-                published = foreskrift_entries[0].published
+                foreskrift_entries=AtomEntry.objects.filter(
+                        myndighetsforeskrift=self).order_by("published")
+                published=foreskrift_entries[0].published
             except AtomEntry.DoesNotExist:
                 print u"Kan inte hitta AtomEntry för föreskrift med id %s" % (self.id,)
 
-        else:
-            # Ny post
-            published = datetime.now()
-
-        # Spara myndighetsföreskriften
+        # Spara myndighetsföreskriften med den vanliga save-metoden.
         super(Myndighetsforeskrift, self).save(*args, **kw)
 
         # Beräkna md5 för dokument och RDF
-        md5 = hashlib.md5()
+        md5=hashlib.md5()
         md5.update(open(self.dokument.path, 'rb').read())
-        dokument_md5 = md5.hexdigest()
+        dokument_md5=md5.hexdigest()
 
-        md5 = hashlib.md5()
-        rdfxml = self.to_rdfxml()
+        md5=hashlib.md5()
+        rdfxml=self.to_rdfxml()
         md5.update(rdfxml.encode('utf-8'))
-        rdf_md5 = md5.hexdigest()
+        rdf_md5=md5.hexdigest()
 
         # Skapa AtomEntry-posten
-        entry = AtomEntry(  myndighetsforeskrift=self,
+        entry=AtomEntry(  myndighetsforeskrift=self,
                             updated=datetime.now(),
                             published=published,
                             entry_id=self.get_absolute_url(),
                             content_md5=dokument_md5,
                             rdf_length=len(rdfxml),
-                            rdf_md5= rdf_md5,
-                         )
+                            rdf_md5=rdf_md5)
 
-        print entry.id
+        # Spara AtomEntry för denna aktivitet
         entry.save()
-        print entry.id
 
-        print entry.to_entryxml()
 
+    def delete(self, *args, **kw):
+        """Override av delete-metoden. I samband med att en föreskrift
+        raderas skriver vi ner ett nytt Atom entry så att konsumenter av feeden
+        får information om att en post raderats."""
+        
+        # PLocka upp ID:t för föreskriften innan den raderas
+        entry_id=self.get_rinfo_uri()
+
+        # Radera myndighetsföreskriften med den vanliga delete-metoden.
+        super(Myndighetsforeskrift, self).delete(*args, **kw)
+
+        # Skapa AtomEntry-posten
+        entry=AtomEntry(    myndighetsforeskrift=None,
+                            updated=datetime.now(),
+                            published=datetime.now(),
+                            content_md5="",
+                            rdf_length=0,
+                            rdf_md5="",
+                            entry_id=entry_id)
+
+        # Spara AtomEntry för denna aktivitet
+        entry.save()
 
     # Några inställningar för beteckningar i admin-gränssnittet
     class Meta:
-        verbose_name = u"Myndighetsföreskrift"
-        verbose_name_plural = u"Myndighetsföreskrifter"
+        verbose_name=u"Myndighetsföreskrift"
+        verbose_name_plural=u"Myndighetsföreskrifter"
 
 
 
 
 class AtomEntry(models.Model):
     """En klass för att skapa ett Atom entry för feeden. Dessa objekt skapas
-    automatiskt i samband med att en föreskrift sparas. Se metoden
-    Myndighetsforeskrift.save()."""
+    automatiskt i samband med att en föreskrift sparas, uppdateras eller
+    raderas. Se metoden Myndighetsforeskrift.save()"""
     
-    myndighetsforeskrift = models.ForeignKey(Myndighetsforeskrift)
-    updated = models.DateTimeField(blank=False)
-    published = models.DateTimeField(blank=False)
-    entry_id = models.CharField(max_length=512, blank=False)
-    content_md5 = models.CharField(max_length=32, blank=False)
-    rdf_length = models.PositiveIntegerField()
-    rdf_md5 = models.CharField(max_length=32, blank=False)
+    myndighetsforeskrift=models.ForeignKey(Myndighetsforeskrift, blank=True)
+    updated=models.DateTimeField(blank=False)
+    published=models.DateTimeField(blank=False)
+    entry_id=models.CharField(max_length=512, blank=False)
+    content_md5=models.CharField(max_length=32, blank=False)
+    rdf_length=models.PositiveIntegerField()
+    rdf_md5=models.CharField(max_length=32, blank=False)
+
 
     def to_entryxml(self):
-        template = loader.get_template('foreskrift_entry.xml')
-        context = Context({ 'foreskrift': self.myndighetsforeskrift,
+        """Skapa en XML-representation av ett entry enligt Atom-standarden. Se
+        mallen i foreskrift_entry.xml"""
+
+        template=loader.get_template('foreskrift_entry.xml')
+        context=Context({ 'foreskrift': self.myndighetsforeskrift,
                             'updated': rfc3339_date(self.updated), 
                             'published': rfc3339_date(self.published), 
                             'entry_id': self.entry_id, 
@@ -200,6 +267,5 @@ class AtomEntry(models.Model):
                             'rdf_length': self.rdf_length, 
                             'rdf_md5': self.rdf_md5, 
                             'rinfo_base_uri': settings.RINFO_BASE_URI,
-                            'rinfo_site_url': settings.RINFO_SITE_URL
-                            })
+                            'rinfo_site_url': settings.RINFO_SITE_URL})
         return template.render(context)
