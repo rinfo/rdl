cddbac19f9dd929fe2ee7a9c851f83b6c8074c4c
pkr <pkr@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2009-05-27 13:45:24 +0000

    Inloggningsexempel med jsecurity
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@646 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- laboratory/applications/rinfo-admin/grails-app/conf/BootStrap.groovy
+++ laboratory/applications/rinfo-admin/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,15 @@
+import org.jsecurity.crypto.hash.Sha1Hash
+
 class BootStrap {
 
-     def init = { servletContext ->
-     }
-     def destroy = {
-     }
+    def init = { servletContext ->
+        // Skapa en exempeladministratör
+        def adminRole = new JsecRole(name: "Administrator").save()
+        def adminUser = new JsecUser(username: "admin", passwordHash: new Sha1Hash("admin").toHex()).save()
+        new JsecUserRoleRel(user: adminUser, role: adminRole).save()
+    }
+    def destroy = {
+    }
+
+
 } 
---
+++ laboratory/applications/rinfo-admin/grails-app/conf/SecurityFilters.groovy
@@ -0,0 +1,32 @@
+class SecurityFilters {
+    def filters = {
+
+        // Ensure that all controllers and actions require an authenticated admin,
+        // except for the "public" controller
+        auth(controller: "*", action: "*") {
+            before = {
+                // Exclude the "public" controller.
+                if (controllerName == "public") return true
+
+                // Rest of a
+                accessControl { role("Administrator") } 
+            } 
+        }
+
+        // Creating, modifying, or deleting a book requires the "Administrator" 
+        // role. 
+        ////bookEditing(controller: "book", action: "(create|edit|save|update|delete)") { 
+        //    before = { 
+        //        accessControl { role("Administrator") } 
+        //    } 
+        //}
+
+        // Showing a book requires the "Administrator" *or* the "User" roles. 
+        //bookShow(controller: "book", action: "show") { 
+        //    before = { 
+        //        accessControl { role("Administrator") || role("User") }
+        //    } 
+        //} 
+    } 
+}
+
