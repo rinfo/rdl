1743ad0b9d6c722db32a67f351368865d7b5412c
pkr <pkr@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2009-06-09 13:55:30 +0000

    Fix svn-problem med Publicationcollection.groovy
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@666 f32dfbcc-ef79-4080-89f5-1451246e9e1e

---
+++ laboratory/applications/rinfo-admin/grails-app/domain/Publicationcollection.groovy
@@ -0,0 +1,30 @@
+class Publicationcollection {
+
+    static constraints = {
+        name(blank:false, maxSize:500)
+        shortname(blank:false, maxSize:500)
+        organization()
+        homepage(url:true, blank:false, maxSize:400)
+        lastUpdated(nullable: true)
+        dateCreated()
+    }
+
+    static belongsTo = Organization
+    Organization organization    
+
+    String name
+    String shortname
+    String homepage
+
+    String toString() { 
+        return name + " (" + shortname + ")"
+    }
+
+    String rinfoURI() {
+        return "http://rinfo.lagrummet.se/collection/" + shortname.toLowerCase().replaceAll(" ", "_")
+    }   
+
+    // Uppdateras automatiskt av Grails
+    Date dateCreated
+    Date lastUpdated
+}
