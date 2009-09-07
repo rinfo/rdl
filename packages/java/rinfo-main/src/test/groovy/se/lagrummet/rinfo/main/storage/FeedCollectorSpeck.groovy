136df8c8d4d8c9c73e94b0ac469e4fbbf65604a0
nlm <nlm@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2009-09-07 15:13:08 +0000

    Added support for providing filename/slug (for enclosure) with a link extension.
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@721 f32dfbcc-ef79-4080-89f5-1451246e9e1e

---
+++ packages/java/rinfo-main/src/test/groovy/se/lagrummet/rinfo/main/storage/FeedCollectorSpeck.groovy
@@ -0,0 +1,65 @@
+package se.lagrummet.rinfo.main.storage
+
+import org.junit.runner.RunWith; import spock.lang.*
+
+import org.apache.abdera.Abdera
+import org.apache.abdera.model.Entry
+import org.apache.abdera.model.Link
+
+import static se.lagrummet.rinfo.main.storage.FeedCollector.LINK_EXT_FILENAME
+import static se.lagrummet.rinfo.main.storage.FeedCollector.getEnclosureSlug
+import static se.lagrummet.rinfo.main.storage.FeedCollector.computeEnclosureSlug
+
+
+@Speck @RunWith(Sputnik) class FeedCollectorSpeck {
+
+    static abdera = Abdera.instance
+
+    def "should collect new since last"() {
+    }
+
+    def "should get enclosure slug from link"() {
+        expect:
+        getEnclosureSlug(link) == expectedSlug
+        where:
+        link << [
+            makeLink("http://example.org/item/one",
+                    "http://localhost/item/one/file.txt"),
+            makeLink("http://example.org/item/one",
+                    "http://localhost/files.cgi?id=123",
+                    "file.txt")
+        ]
+        expectedSlug << [
+            "/item/one/file.txt",
+            "/item/one/file.txt"
+        ]
+    }
+
+    def "should compute enclosure slug from URLs"() {
+        expect:
+        computedSlug == expectedSlug
+        where:
+        computedSlug << [
+            computeEnclosureSlug(new URI("http://example.org/item/one"),
+                new URI("http://localhost/item/one/file.txt"))
+        ]
+        expectedSlug << [ "/item/one/file.txt" ]
+    }
+
+    private Link makeLink(entryId, href, ext=null) {
+        def entry = abdera.newEntry()
+        entry.setId(entryId)
+        def link = entry.addLink(href)
+        if (ext) link.setAttributeValue(LINK_EXT_FILENAME, ext)
+        return link
+    }
+
+    /* TODO:
+    shouldSortUnsortedFeedPage
+    shouldVerifyMd5AndLength
+
+    shouldFailAndLogOnMissingRdf
+    ...
+    */
+
+}
