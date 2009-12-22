35c60dda1773cff5fe49e93f1e7215efb1c6fc0c
nlm <nlm@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2009-12-22 12:09:23 +0000

    sources to download in separate config (json)
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@882 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- manage/project/download.py
+++ manage/project/download.py
@@ -1,68 +1,56 @@
 from __future__ import with_statement
 import urllib2
-import os
 import time
 import shutil
-import functools as f
+from os import stat, path as p
+try: import simplejson as json
+except ImportError: import json
 
 
-projectroot = os.path.join(os.path.dirname(__file__), '../../')
+scriptdir = p.dirname(__file__)
+sourcefile = p.join(scriptdir, "download.json")
+basedir = p.dirname(p.abspath(sourcefile))
 
-
-def download_models(usemodtime=True):
-    destdir = projectroot+"resources/external/rdf/"
-    get = f.partial(_http_get, usemodtime=_unrepr(usemodtime),
-             headers=[('Accept', "application/rdf+xml")])
-
-    # W3C Standards
-    get("http://www.w3.org/1999/02/22-rdf-syntax-ns#", destdir+"rdf.rdfs")
-    get("http://www.w3.org/2000/01/rdf-schema#", destdir+"rdf-schema.rdfs")
-    get("http://www.w3.org/2002/07/owl#", destdir+"owl.owl")
-    #get("http://www.w3.org/2001/XMLSchema#", destdir+"xsd.rdfs") # 404:s..
-    get("http://www.w3.org/2004/02/skos/core#", destdir+"skos_core.rdfs")
-
-    # Standard Community Data
-    get("http://purl.org/dc/terms/", destdir+"dcterms.rdfs")
-    get("http://purl.org/dc/elements/1.1/", destdir+"dc_elements.rdfs")
-
-    # De-facto Community Data
-    get("http://xmlns.com/foaf/0.1/", destdir+"foaf.owl")
-    get("http://rdfs.org/sioc/ns#", destdir+"sioc.owl")
-    get("http://purl.org/ontology/bibo/", destdir+"bibo.owl")
-
-    # Experimental Community Data
-    get("http://bblfish.net/work/atom-owl/2006-06-06/#", destdir+"atomowl.owl")
-
-    # Legacy(?) Community Data
-    #get("http://prismstandard.org/namespaces/1.2/basic/",
-    #        destdir+"prism.rdfs") # 404:s..
-
-def download_xslt(usemodtime=True):
-    destdir = projectroot+"resources/external/xslt/"
-    get = f.partial(_http_get, usemodtime=_unrepr(usemodtime))
-    get("http://purl.org/oort/impl/xslt/grit/rdfxml-grit.xslt", destdir)
-
-def download_all(usemodtime=True):
-    download_models(usemodtime)
-    download_xslt(usemodtime)
-
+def download(match=None, usemodtime=True):
+    usemodtime = _unrepr(usemodtime)
+    with open(sourcefile) as f:
+        downloads = json.load(f)
+    if match and match in downloads:
+        groupname = match
+        match = None
+    else:
+        groupname = None
+    for name, group in downloads.items():
+        if groupname and groupname != name:
+            continue
+        print "[Group: %s]" % name
+        destdir = p.normpath(p.join(basedir, group.get('destination', "")))
+        headers = (group.get('headers') or {}).items()
+        named = group.get('named') or {}
+        items = group.get('items') or ()
+        resources = ( [(url, p.join(destdir, name))
+                       for name, url in named.items()] +
+                      [(url, destdir) for url in items] )
+        for url, dest in resources:
+            if match and match not in url:
+                continue
+            _http_get(url, dest, usemodtime=usemodtime, headers=headers)
 
 _unrepr = lambda v: eval(v) if isinstance(v, str) else v
 
-
 def _http_get(url, dest, usemodtime=True, headers=()):
-    if dest.endswith('/'):
-        dest += url.rsplit('/', 1)[-1]
-    print "Download <%(url)s> to <%(dest)s>"%vars()
+    if p.isdir(dest):
+        dest = p.join(dest, url.rsplit('/', 1)[-1])
+    print "Downloading <%(url)s> to <%(dest)s> ..."%vars(),
     req = urllib2.Request(url)
     for header, value in headers:
         req.add_header(header, value)
-    if os.path.exists(dest):
+    if p.exists(dest):
         if not usemodtime:
             print "Destination exists, skipping."
             return
         modstamp = time.strftime("%a, %d %b %Y %H:%M:%S GMT",
-                time.gmtime(os.stat(dest).st_mtime))
+                time.gmtime(stat(dest).st_mtime))
         req.add_header('If-Modified-Since', modstamp)
     try:
         res = urllib2.urlopen(req)
@@ -70,6 +58,7 @@ def _http_get(url, dest, usemodtime=True, headers=()):
         print e
         return
     print res.info()
+    print "Done."
     with file(dest, 'w') as out:
         shutil.copyfileobj(res, out)
 
