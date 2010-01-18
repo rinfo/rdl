11f2d3c1f4fe551f6953ab2f2005359f355f8790
nlm <nlm@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2010-01-18 18:59:23 +0000

    added fh:complete tag
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@888 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- laboratory/applications/rinfo-admin/grails-app/controllers/EntryController.groovy
+++ laboratory/applications/rinfo-admin/grails-app/controllers/EntryController.groovy
@@ -21,6 +21,7 @@ class EntryController {
         def mb = new groovy.xml.MarkupBuilder(sw)
 
         mb.feed(xmlns: "http://www.w3.org/2005/Atom",
+                'xmlns:fh':"http://purl.org/syndication/history/1.0",
                 'xmlns:at':"http://purl.org/atompub/tombstones/1.0",
                 'xmlns:le':"http://purl.org/atompub/link-extensions/1.0",
                 'xmlns:rdf':"http://www.w3.org/1999/02/22-rdf-syntax-ns#") {
@@ -30,24 +31,26 @@ class EntryController {
                 author{ name("Rinfo") }
                 id("tag:admin.lagrummet.se,2009:data")
 
-                entryList.each { item ->
-                    if(!item.dateDeleted) {
-                        entry {
-                            title(type:"text", item.title)
-                            published(df.format(item.dateCreated))
-                            updated(df.format(item.lastUpdated))
-                            author{name("Rinfo")}
-                            id(item.uri)
-                            content(type: "application/rdf+xml") {
-                                mb.yieldUnescaped item.content
-                            }
-                        }
-                    } else {
-                        'at:deleted-entry'(ref: item.uri, when: df.format(item.dateDeleted)) {
-                            'at:comment'("Post med URI " + item.uri + " raderades.")
+            'fh:complete'()
+
+            entryList.each { item ->
+                if(!item.dateDeleted) {
+                    entry {
+                        title(type:"text", item.title)
+                        published(df.format(item.dateCreated))
+                        updated(df.format(item.lastUpdated))
+                        author{name("Rinfo")}
+                        id(item.uri)
+                        content(type: "application/rdf+xml") {
+                            mb.yieldUnescaped item.content
                         }
                     }
+                } else {
+                    'at:deleted-entry'(ref: item.uri, when: df.format(item.dateDeleted)) {
+                        'at:comment'("Post med URI " + item.uri + " raderades.")
+                    }
                 }
+            }
         }
 
         response.setContentType("application/atom+xml")
