178c016d73b0f5435f1be28529838f32f533eaee
gfa <gfa@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2010-09-06 14:56:51 +0000

    removed redundant final keyword and made the error message more specific
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@1209 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- packages/java/rinfo-rdf-repo/src/main/java/se/lagrummet/rinfo/rdf/repo/RepositoryHandlerFactory.java
+++ packages/java/rinfo-rdf-repo/src/main/java/se/lagrummet/rinfo/rdf/repo/RepositoryHandlerFactory.java
@@ -4,31 +4,31 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 
-
 public class RepositoryHandlerFactory {
 
     /**
      * Create a new RepositoryHandler from the provided configuration.
      */
-    public static final RepositoryHandler create(Configuration config) throws Exception {
+    public static RepositoryHandler create(Configuration config) throws Exception {
         String repoId = config.getString("repositoryId");
         String inferenceType = config.getString("inferenceType");
         String storeType = config.getString("storeType").toLowerCase();
-
+        
         String serverUrl = config.getString("remote.serverUrl");
         if (!StringUtils.isEmpty(serverUrl)) {
             return new RemoteRepositoryHandler(serverUrl, repoId,
                     storeType, inferenceType);
         }
-
+        
         String dataDir = config.getString("native.dataDir");
         if (!StringUtils.isEmpty(dataDir)) {
             return new LocalRepositoryHandler(dataDir, repoId,
                     storeType, inferenceType);
         }
-
+        
         throw new ConfigurationException(
-                "Not enough properties to determine which RepositoryHandler to create.");
+                "Not enough properties to determine which RepositoryHandler to create." +
+                "Need one of 'remote.serverUrl' or 'native.dataDir'");
     }
-
+    
 }
