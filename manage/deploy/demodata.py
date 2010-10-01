57f562ddca327e0f93114c1ce5ccb604d4d2b56e
nlm <nlm@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2010-10-01 19:39:15 +0000

    cleanup
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@1246 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- manage/deploy/demodata.py
+++ manage/deploy/demodata.py
@@ -1,20 +1,23 @@
+from __future__ import with_statement
 from fabric.api import env, local, cd
 from fabric.contrib.files import *
+
 
 env.java_opts = 'JAVA_OPTS="-Xms512Mb -Xmx1024Mb"'
 env.test_data_dir = "/opt/_workapps/rinfo/testdata"
 env.test_data_tools = "../tools/testscenarios"
 
+
 def download_demodata():
     _mkdir_keep_prev("%(test_data_dir)s/lagen-nu"%env)
-    
+
     with cd("%(test_data_dir)s/lagen-nu"%env):
         local("curl https://lagen.nu/sfs/parsed/rdf.nt -o lagennu-sfs.nt")
         #local("curl https://lagen.nu/dv/parsed/rdf.nt -o lagennu-dv.nt")
 
 def demodata_to_depot():
     _mkdir_keep_prev("%(test_data_dir)s/sfs-depot"%env)
-    
+
     local("%(java_opts)s groovy %(test_data_tools)s/n3dump_to_depot.groovy %(test_data_dir)s/lagen-nu/lagennu-sfs.nt %(test_data_dir)s/sfs-depot"%env)
 
 #def serve_depot(depot=None, port=8180):
@@ -26,8 +29,9 @@ def demodata_to_depot():
 def _mkdir_keep_prev(dir_path):
     if exists("%s-prev"%dir_path):
         local("rm -rf %s-prev"%dir_path)
-    
+
     if exists("%s"%dir_path):
         local("mv %s %s-prev"%(dir_path, dir_path))
-    
+
     local("mkdir -p %s"%dir_path)
+
