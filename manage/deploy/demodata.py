57e9daa46007d69191887ba5ede7dc7e1a2b3a82
gfa <gfa@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2010-10-06 11:41:37 +0000

    The demo data from lagen.nu (dv and sfs) can now be downloaded and transformed into depots using Fabric.
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@1257 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- manage/deploy/demodata.py
+++ manage/deploy/demodata.py
@@ -8,17 +8,54 @@ env.test_data_dir = "/opt/_workapps/rinfo/testdata"
 env.test_data_tools = "../tools/testscenarios"
 
 
-def download_demodata():
-    _mkdir_keep_prev("%(test_data_dir)s/lagen-nu"%env)
+def demo_data_download_sfs():
+    _mkdir_keep_prev("%(test_data_dir)s/lagen-nu-sfs"%env)
 
-    with cd("%(test_data_dir)s/lagen-nu"%env):
+    with cd("%(test_data_dir)s/lagen-nu-sfs"%env):
         local("curl https://lagen.nu/sfs/parsed/rdf.nt -o lagennu-sfs.nt")
-        #local("curl https://lagen.nu/dv/parsed/rdf.nt -o lagennu-dv.nt")
 
-def demodata_to_depot():
+def demo_data_to_depot_sfs():
     _mkdir_keep_prev("%(test_data_dir)s/sfs-depot"%env)
 
-    local("%(java_opts)s groovy %(test_data_tools)s/n3dump_to_depot.groovy %(test_data_dir)s/lagen-nu/lagennu-sfs.nt %(test_data_dir)s/sfs-depot"%env)
+    local("%(java_opts)s groovy %(test_data_tools)s/n3dump_to_depot.groovy %(test_data_dir)s/lagen-nu-sfs/lagennu-sfs.nt %(test_data_dir)s/sfs-depot"%env)
+
+def demo_data_upload_sfs():
+    print "Uploading demo data is not implemented yet"
+
+def demo_data_serve_sfs():
+    print "Serving demo data is not implemented yet"
+
+def demo_refresh_sfs():
+    """Downloads, transforms, uploads and starts to serve the demo data"""
+    demo_data_download_sfs()
+    demo_data_to_depot_sfs()
+    demo_data_upload_sfs()
+    demo_data_serve_sfs()
+
+
+def demo_data_download_dv():
+    _mkdir_keep_prev("%(test_data_dir)s/lagen-nu-dv"%env)
+
+    with cd("%(test_data_dir)s/lagen-nu-dv"%env):
+        local("curl https://lagen.nu/dv/parsed/rdf.nt -o lagennu-dv.nt")
+
+def demo_data_to_depot_dv():
+    _mkdir_keep_prev("%(test_data_dir)s/dv-depot"%env)
+
+    local("%(java_opts)s groovy %(test_data_tools)s/n3dump_to_depot.groovy %(test_data_dir)s/lagen-nu-dv/lagennu-dv.nt %(test_data_dir)s/dv-depot"%env)
+
+def demo_data_upload_dv():
+    print "Uploading demo data is not implemented yet"
+
+def demo_data_serve_dv():
+    print "Serving demo data is not implemented yet"
+
+def demo_refresh_dv():
+    """Downloads, transforms, uploads and starts to serve the demo data"""
+    demo_data_download_dv()
+    demo_data_to_depot_dv()
+    demo_data_upload_dv()
+    demo_data_serve_dv()
 
 #def serve_depot(depot=None, port=8180):
 #    depot = depot or "%s/lagen-nu/%s" % (env.test_data_dir, "sfs-depot")
