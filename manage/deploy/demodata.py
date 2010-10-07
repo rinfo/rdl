a26b23fc590f383f0f9f6de8a455144857f3006c
gfa <gfa@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2010-10-07 15:30:53 +0000

    Refactored the tasks so that they take the dataset as a parameter to limit the number of Fabric tasks.
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@1265 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- manage/deploy/demodata.py
+++ manage/deploy/demodata.py
@@ -4,79 +4,63 @@ from fabric.contrib.files import *
 from fabric.contrib.project import rsync_project
 from targetenvs import _needs_targetenv
 from deploy import _deploy_war
-
+from util import venv
 
 env.java_opts = 'JAVA_OPTS="-Xms512Mb -Xmx1024Mb"'
 env.test_data_dir = "/opt/_workapps/rinfo/testdata"
 env.test_data_tools = "../tools/testscenarios"
 
+lagen_nu_datasets = ('sfs', 'dv')
+riksdagen_se_datasets = ('prop', 'sou', 'ds')
 
-def demo_data_download_sfs():
-    _mkdir_keep_prev("%(test_data_dir)s/lagen-nu-sfs"%env)
+def demo_data_download(dataset):
+    """Downloads a demo dataset from its source"""
+    _mkdir_keep_prev("%(test_data_dir)s/%(dataset)s-raw"%venv())
 
-    with cd("%(test_data_dir)s/lagen-nu-sfs"%env):
-        local("curl https://lagen.nu/sfs/parsed/rdf.nt -o lagennu-sfs.nt")
+    with cd("%(test_data_dir)s/%(dataset)s-raw"%venv()):
+        if dataset in lagen_nu_datasets:
+            _download_lagen_nu_data(dataset)
+        else if dataset in riksdagen_se_datasets:
+            raise NotImplementedError
+        else:
+            raise ValueError("Dataset '%s' is not known to me"%dataset)
 
-def demo_data_to_depot_sfs():
-    _mkdir_keep_prev("%(test_data_dir)s/sfs"%env)
+def demo_data_to_depot(dataset):
+    """Transforms the downloaded demo data to a depot"""
+    _mkdir_keep_prev("%(test_data_dir)s/%(dataset)s"%venv())
 
-    local("%(java_opts)s groovy %(test_data_tools)s/n3dump_to_depot.groovy %(test_data_dir)s/lagen-nu-sfs/lagennu-sfs.nt %(test_data_dir)s/sfs"%env)
+    if dataset in lagen_nu_datasets:
+        _transform_lagen_nu_data(dataset)
+    else if dataset in riksdagen_se_datasets:
+        raise NotImplementedError
+    else:
+        raise ValueError("Dataset '%s' is not known to me"%dataset)
 
-def demo_data_upload_sfs():
+def demo_data_upload(dataset):
+    """Uploads the transformed demo data depot to the demo server"""
     _needs_targetenv()
-    rsync_project((env.demo_data_root), "%(test_data_dir)s/sfs"%env, exclude=".*", delete=True)
+    rsync_project((env.demo_data_root), "%(test_data_dir)s/%(dataset)s"%venv(), exclude=".*", delete=True)
 
-def demo_build_war_sfs():
+def demo_build_war(dataset):
+    """Builds a webapp capable of serving an uploaded demo data depot"""
     local("cd %(java_packages)s/demodata-supply && "
-            "mvn -Ddataset=sfs -Ddemodata-root=%(demo_data_root)s clean package"%env, capture=False)
+            "mvn -Ddataset=%(dataset)s -Ddemodata-root=%(demo_data_root)s clean package"%venv(), capture=False)
 
-def demo_deploy_war_sfs():
+def demo_deploy_war(dataset):
+    """Deploys an uploaded demo webapp"""
     if not exists(env.dist_dir):
         run("mkdir %(dist_dir)s"%env)
-    _deploy_war("%(java_packages)s/demodata-supply/target/sfs-demodata-supply.war"%env,
-            "sfs-demodata-supply")
+    _deploy_war("%(java_packages)s/demodata-supply/target/%(dataset)s-demodata-supply.war"%venv(),
+            "%(dataset)s-demodata-supply"%venv())
 
-def demo_refresh_sfs():
-    """Downloads, transforms, uploads and starts to serve the demo data"""
-    demo_data_download_sfs()
-    demo_data_to_depot_sfs()
-    demo_data_upload_sfs()
-    demo_build_war_sfs()
-    demo_deploy_war_sfs()
+def demo_refresh(dataset):
+    """Downloads, transforms, uploads, builds and deploys a webapp for serving a demo dataset"""
+    demo_data_download(dataset)
+    demo_data_to_depot(dataset)
+    demo_data_upload(dataset)
+    demo_build_war(dataset)
+    demo_deploy_war(dataset)
 
-
-def demo_data_download_dv():
-    _mkdir_keep_prev("%(test_data_dir)s/lagen-nu-dv"%env)
-
-    with cd("%(test_data_dir)s/lagen-nu-dv"%env):
-        local("curl https://lagen.nu/dv/parsed/rdf.nt -o lagennu-dv.nt")
-
-def demo_data_to_depot_dv():
-    _mkdir_keep_prev("%(test_data_dir)s/dv"%env)
-
-    local("%(java_opts)s groovy %(test_data_tools)s/n3dump_to_depot.groovy %(test_data_dir)s/lagen-nu-dv/lagennu-dv.nt %(test_data_dir)s/dv"%env)
-
-def demo_data_upload_dv():
-    _needs_targetenv()
-    rsync_project((env.demo_data_root), "%(test_data_dir)s/dv"%env, exclude=".*", delete=True)
-
-def demo_build_war_dv():
-    local("cd %(java_packages)s/demodata-supply && "
-            "mvn -Ddataset=dv -Ddemodata-root=%(demo_data_root)s clean package"%env, capture=False)
-
-def demo_deploy_war_dv():
-    if not exists(env.dist_dir):
-        run("mkdir %(dist_dir)s"%env)
-    _deploy_war("%(java_packages)s/demodata-supply/target/dv-demodata-supply.war"%env,
-            "dv-demodata-supply")
-
-def demo_refresh_dv():
-    """Downloads, transforms, uploads and starts to serve the demo data"""
-    demo_data_download_dv()
-    demo_data_to_depot_dv()
-    demo_data_upload_dv()
-    demo_build_war_dv()
-    demo_deploy_war_dv()
 
 #def serve_depot(depot=None, port=8180):
 #    depot = depot or "%s/lagen-nu/%s" % (env.test_data_dir, "sfs")
@@ -92,3 +76,9 @@ def _mkdir_keep_prev(dir_path):
         local("mv %s %s-prev"%(dir_path, dir_path))
 
     local("mkdir -p %s"%dir_path)
+
+def _download_lagen_nu_data(dataset):
+    local("curl https://lagen.nu/%s/parsed/rdf.nt -o lagennu-%s.nt"%(dataset,dataset))
+
+def _transform_lagen_nu_data(dataset):
+    local("%(java_opts)s groovy %(test_data_tools)s/n3dump_to_depot.groovy %(test_data_dir)s/%(dataset)s-raw/lagennu-%(dataset)s.nt %(test_data_dir)s/sfs"%venv())
