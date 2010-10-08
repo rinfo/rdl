ad7b37e1fc6a1d113e28ce066f9bb1875c7e26ac
gfa <gfa@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2010-10-08 09:53:12 +0000

    Added note about how to install Java on Ubuntu.
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@1268 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- manage/sysconf/install/1_deps.sh
+++ manage/sysconf/install/1_deps.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 sudo apt-get install apache2
 sudo /usr/sbin/a2enmod proxy_ajp
-#http://www.debian.org/doc/manuals/debian-java-faq/ch7.html ::
+# To install sun-java6-jdk you need to:
+# 
+# For Debian:
+#   http://www.debian.org/doc/manuals/debian-java-faq/ch7.html ::
 #   sudo vim /etc/apt/sources.list ::
 #       deb <$HTTP-FTP...> $DIST main contrib non-free
 #   sudo apt-get update
+#
+# For Ubuntu:
+#   sudo add-apt-repository "deb http://archive.canonical.com/ lucid partner
+#   sudo apt-get update
+#   sudo apt-get install sun-java6-jdk
 sudo apt-get install sun-java6-jdk
 sudo apt-get install pgp
