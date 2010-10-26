eaed8d7bd2749f9a2f412f7981a38e35e7640d68
nlm <nlm@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2010-10-26 13:27:49 +0000

    manage: added simple ping_collector
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@1309 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- manage/apptools.py
+++ manage/apptools.py
@@ -4,15 +4,18 @@ Application Tools
 from fabric.api import *
 
 
-def ping_main_collector():
+def ping_collector(collector_url, feed_url):
+    local("curl --data 'feed=%(feed_url)s' %(collector_url)s"%vars())
+
+def ping_main_collector(feed_url):
     #require('roledefs', provided_by=targetenvs)
     collector_url = "http://%s/collector/" % env.roledefs['main'][0]
-    feed_url = "http://%s:8182/feed/current" % env.roledefs['examples'][0]
-    local("curl --data 'feed=%(feed_url)s' %(collector_url)s"%vars())
+    #feed_url = "http://%s:8182/feed/current" % env.roledefs['examples'][0]
+    ping_main_collector(collector_url, feed_url)
 
 def ping_service_collector():
     #require('roledefs', provided_by=targetenvs)
     collector_url = "http://%s/collector/" % env.roledefs['service'][0]
     feed_url = "http://%s/feed/current" % env.roledefs['main'][0]
-    local("curl --data 'feed=%(feed_url)s' %(collector_url)s"%vars())
+    ping_main_collector(collector_url, feed_url)
 
