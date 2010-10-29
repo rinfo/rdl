40f4b6f8b71db7a13131b74b867a922b5d043b1b
nlm <nlm@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2010-10-29 16:09:38 +0000

    depot: pass along lockedEntry in LockedDepotEntryException
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@1332 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- packages/java/rinfo-store/src/main/java/se/lagrummet/rinfo/store/depot/LockedDepotEntryException.java
+++ packages/java/rinfo-store/src/main/java/se/lagrummet/rinfo/store/depot/LockedDepotEntryException.java
@@ -2,8 +2,15 @@ package se.lagrummet.rinfo.store.depot;
 
 public class LockedDepotEntryException extends DepotReadException {
 
+    private DepotEntry lockedEntry;
+
     public LockedDepotEntryException(DepotEntry depotEntry) {
         super(depotEntry+" is locked!");
+        lockedEntry = depotEntry;
+    }
+
+    public DepotEntry getLockedEntry() {
+        return lockedEntry;
     }
 
 }
