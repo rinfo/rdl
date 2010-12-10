96f79183a7dd26aeeec73cfaae5493968e575f3c
nlm <nlm@f32dfbcc-ef79-4080-89f5-1451246e9e1e>
2010-12-10 13:29:10 +0000

    manage: Added flag to package_main and main_all for optionally turning off the running of tests.
    git-svn-id: https://dev.lagrummet.se/svn/rinfo/trunk@1407 f32dfbcc-ef79-4080-89f5-1451246e9e1e

--- manage/deploy/rinfo_main.py
+++ manage/deploy/rinfo_main.py
@@ -1,5 +1,6 @@
 from fabric.api import *
 from fabric.contrib.files import exists
+from util import venv
 from deploy import local_lib_rinfo_pkg, _deploy_war
 from targetenvs import _needs_targetenv
 
@@ -7,11 +8,12 @@ from targetenvs import _needs_targetenv
 # Local build
 
 @runs_once
-def package_main(deps="1"):
+def package_main(deps="1", test="1"):
     if int(deps): local_lib_rinfo_pkg()
     _needs_targetenv()
+    flags = "" if int(test) else "-Dmaven.test.skip=true"
     local("cd %(java_packages)s/rinfo-main/ && "
-            "mvn -P%(target)s clean package war:war"%env, capture=False)
+            "mvn %(flags)s -P%(target)s clean package war:war" % venv(), capture=False)
 
 ##
 # Server deploy
@@ -20,22 +22,22 @@ def package_main(deps="1"):
 @roles('main')
 def setup_main():
     if not exists(env.dist_dir):
-        run("mkdir %(dist_dir)s"%env)
+        run("mkdir %(dist_dir)s" % env)
     if not exists(env.rinfo_dir):
-        sudo("mkdir %(rinfo_dir)s"%env)
+        sudo("mkdir %(rinfo_dir)s" % env)
     if not exists(env.rinfo_main_store):
-        sudo("mkdir %(rinfo_main_store)s"%env)
-        sudo("chown -R %(tomcat_user)s %(rinfo_main_store)s"%env)
+        sudo("mkdir %(rinfo_main_store)s" % env)
+        sudo("chown -R %(tomcat_user)s %(rinfo_main_store)s" % env)
 
 @roles('main')
 def deploy_main():
     setup_main()
-    _deploy_war("%(java_packages)s/rinfo-main/target/rinfo-main-%(target)s.war"%env,
+    _deploy_war("%(java_packages)s/rinfo-main/target/rinfo-main-%(target)s.war" % env,
             "rinfo-main")
 
 @roles('main')
-def main_all(deps="1"):
-    package_main(deps)
+def main_all(deps="1", test="1"):
+    package_main(deps, test)
     deploy_main()
 
 ##
@@ -45,5 +47,5 @@ def main_all(deps="1"):
 def clear_main_collect_log(force="0"):
     raise NotImplementedError
     # TODO: make **SURE** this is whay you really want to do!
-    #sudo("rm -rf %(rinfo_main_store)s/collector-log"%env, user=env.tomcat_user)
+    #sudo("rm -rf %(rinfo_main_store)s/collector-log" % env, user=env.tomcat_user)
 
