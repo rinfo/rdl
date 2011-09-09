0870c1415fb3f6de0f273ecc7fabdeaf0ae40fc1
Niklas Lindström <lindstream@gmail.com>
2011-09-09 16:19:42 +0200

    JSONLDSerializer improvements
    * Flag params for keepUnmapped and addRevs.
    * Support for combined type token and data.
    * Some code cleanup.

--- packages/java/rinfo-base/src/test/groovy/se/lagrummet/rinfo/base/rdf/jsonld/JSONLDSerializerSpec.groovy
+++ packages/java/rinfo-base/src/test/groovy/se/lagrummet/rinfo/base/rdf/jsonld/JSONLDSerializerSpec.groovy
@@ -11,10 +11,17 @@ class JSONLDSerializerSpec extends Specification {
     def repo
     def describer
 
+    def personIRI = "http://example.org/person/someone#self"
+    def homepageIRI = "http://example.org/person/someone"
+
     def setup() {
         repo = RDFUtil.createMemoryRepository()
         describer = new Describer(repo.getConnection())
         describer.setPrefix("foaf", FOAF)
+        def person = describer.newDescription(personIRI, "foaf:Person")
+        person.addLiteral("foaf:name", "Some One")
+        def homepage = person.addRel("foaf:homepage", homepageIRI)
+        homepage.addType("foaf:Document")
     }
 
     def cleanup() {
@@ -22,7 +29,6 @@ class JSONLDSerializerSpec extends Specification {
     }
 
     def "should serialize RDF as JSON"() {
-        // TODO: split into separate tests
         given:
         def context = [
             iri: "@subject",
@@ -37,13 +43,6 @@ class JSONLDSerializerSpec extends Specification {
         ]
         and:
         def serializer = new JSONLDSerializer(context)
-        and:
-        def personIRI = "http://example.org/person/someone#self"
-        def homepageIRI = "http://example.org/person/someone"
-        def person = describer.newDescription(personIRI, "foaf:Person")
-        person.addLiteral("foaf:name", "Some One")
-        def homepage = person.addRel("foaf:homepage", homepageIRI)
-        homepage.addType("foaf:Document")
 
         when:
         def data = serializer.toJSON(repo, personIRI)
@@ -53,17 +52,23 @@ class JSONLDSerializerSpec extends Specification {
         data.name == "Some One"
         data.homepage.iri == homepageIRI
         data.homepage.type == 'Document'
+    }
 
-        // NOTE: *experimental* @rev support
+    def "should support experimental @rev notation"() {
+        given:
+        def context = [
+            homepage: "${FOAF}homepage"
+        ]
+        def serializer = new JSONLDSerializer(context, false, true)
         when: "serializing homepage"
         def hpData = serializer.toJSON(repo, homepageIRI)
         and:
         def rev = hpData['@rev']
         then: "data should contain reverse relation to person"
         rev.homepage.size() == 1
-        rev.homepage[0].iri == personIRI
+        rev.homepage[0]['@subject'] == personIRI
         and: "roto resource in reverse item only contains subject"
-        rev.homepage[0].homepage == [iri: homepageIRI]
+        rev.homepage[0].homepage == ['@subject': homepageIRI]
     }
 
     def "should use default vocab"() {
@@ -72,14 +77,31 @@ class JSONLDSerializerSpec extends Specification {
             "@vocab": FOAF
         ]
         and:
-        def topicIRI = "http://example.org/person/someone#self"
         def serializer = new JSONLDSerializer(context)
-        describer.newDescription(topicIRI, "foaf:Person")
+        describer.newDescription(personIRI, "foaf:Person")
         when:
-        def data = serializer.toJSON(repo, topicIRI)
+        def data = serializer.toJSON(repo, personIRI)
         then:
         serializer.vocab == FOAF
         data['@type'] == 'Person'
+    }
+
+    def "should add both type token and type data if specified"() {
+        given:
+        def context = [
+            "a": "@type",
+            "type": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+            "@vocab": FOAF
+        ]
+        and:
+        def serializer = new JSONLDSerializer(context)
+        describer.newDescription(personIRI, "foaf:Person")
+        when:
+        def data = serializer.toJSON(repo, personIRI)
+        then:
+        serializer.vocab == FOAF
+        data['a'] == 'Person'
+        data['type']['@subject'] == "${FOAF}Person"
     }
 
 }
