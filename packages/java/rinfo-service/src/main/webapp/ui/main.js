5e08aed1818b2183951870bdec7b9b83638c4c52
Niklas Lindström <lindstream@gmail.com>
2011-09-23 15:43:08 +0200

    Add service UI document view and improve general interaction

--- packages/java/rinfo-service/src/main/webapp/ui/main.js
+++ packages/java/rinfo-service/src/main/webapp/ui/main.js
@@ -1,11 +1,57 @@
 
 var base = "";
 
+/* main */
+
 $(function () {
 
+  loadStats();
+
+  $('#queryForm').submit(function () {
+    var $self = $(this);
+    var serviceRef = $self.attr('action') +'?'+ $self.serialize();
+    window.location.hash = serviceRef;
+    queryService(serviceRef);
+    return false;
+  });
+
+  $('#resultsView a:link, a.svc:link').live('click', function () {
+    var serviceRef = $(this).attr('href').substring(1);
+    if (serviceRef) {
+      window.location.hash = serviceRef;
+      queryService(serviceRef);
+    }
+    return false;
+  });
+
+  var serviceRef = window.location.hash.substring(1);
+  queryService(serviceRef);
+
+});
+
+/* lookup */
+
+function queryService(serviceRef) {
+  if (!serviceRef) {
+    return;
+  }
+  loader.start($('#content'));
+  $.getJSON(base + serviceRef, function (data) {
+    loader.stop($('#content'));
+    (data.itemsPerPage? renderResults : renderDocument)(serviceRef, data);
+  }).error(function (response) {
+    loader.stop($('#content'));
+    renderError(serviceRef, response);
+  });
+}
+
+function loadStats() {
+  loader.start($('#queryBox'));
   $.getJSON(base + '/-/stats', function (stats) {
+    loader.stop($('#queryBox'));
     $.each(stats.slices, function () {
       var $select = $('#' + this.dimension);
+      $select.empty();
       $.each(this.observations, function () {
         if (!this.term) return; // TODO: date stats
         var key = this.term;
@@ -17,56 +63,68 @@ $(function () {
       });
     });
   });
-
-  $('#queryForm').submit(function () {
-    var $self = $(this);
-    var serviceRef = $self.attr('action') +'?'+ $self.serialize();
-    window.location.hash = serviceRef;
-    queryService(serviceRef);
-    return false;
-  });
-
-  $('#resultInfo .pagination a:link').live('click', function () {
-    var $self = $(this);
-    var ref = $self.attr('href');
-    var ref = ref.substring(1);
-    if (ref) {
-      window.location.hash = ref;
-      queryService(ref);
-    }
-    return false;
-  });
-
-  var serviceRef = window.location.hash.substring(1);
-  queryService(serviceRef);
-
-});
-
-function queryService(serviceRef) {
-  $.getJSON(base + serviceRef, function (results) {
-
-    // TODO: compute pagination
-    //results.startIndex
-    //results.totalResults
-    //results.itemsPerPage
-
-    var endIndex = results.startIndex + results.itemsPerPage;
-    if (endIndex > results.totalResults) {
-      endIndex = results.totalResults;
-    }
-
-    $('#resultInfoTemplate').tmpl({
-      queryStr: serviceRef,
-      start: results.startIndex + 1,
-      end: endIndex,
-      totalResults: results.totalResults,
-      results: results
-    }).appendTo($('#resultInfo').empty());
-
-    var $tbody = $('#resultRows')
-    $tbody.empty();
-    $('#resultRowTemplate').tmpl(results.items).appendTo($tbody);
-
-  });
 }
+
+/* render */
+
+function renderResults(serviceRef, results) {
+  var endIndex = results.startIndex + results.itemsPerPage;
+  if (endIndex > results.totalResults) {
+    endIndex = results.totalResults;
+  }
+  $('#errorInfo').empty();
+  $('#documentView').empty();
+  $('#resultsTemplate').tmpl({
+    queryStr: serviceRef,
+    start: results.startIndex + 1,
+    end: endIndex,
+    totalResults: results.totalResults,
+    results: results
+  }).appendTo($('#resultsView').removeClass('folded').empty());
+}
+
+function renderDocument(serviceRef, doc) {
+  $('#errorInfo').empty();
+  $('#resultsView:has(*)').addClass('folded');
+  $('#documentTemplate').tmpl({
+    obj: doc
+  }).appendTo($('#documentView').empty());
+}
+
+function renderError(serviceRef, response) {
+  $('#documentView').empty();
+  $('#errorTemplate').tmpl({
+    serviceRef: serviceRef,
+    response: response
+  }).appendTo($('#errorInfo').empty());
+}
+
+/* utils */
+
+function toServiceRef(iri) {
+  return iri.replace(/^https?:\/\/[^\/]+(.+?)(\/data\.json)?$/, "$1") + "/data.json";
+}
+
+/**
+ * Throbbing loader indicator.
+ */
+var loader = new function () {
+
+  var dur = this.dur = 1000;
+
+  this.start = function (o) {
+    return fadeOut.apply(o.addClass('loading')[0]);
+  }
+  this.stop = function (o) {
+    return o.stop(true).removeClass('loading').css('opacity', 1);
+  }
+
+  function fadeOut() {
+    $(this).animate({'opacity': 0.2}, {queue: true, duration: dur, complete: fadeIn});
+  }
+  function fadeIn() {
+    $(this).animate({'opacity': 1}, {queue: true, duration: dur, complete: fadeOut});
+  }
+
+};
 
