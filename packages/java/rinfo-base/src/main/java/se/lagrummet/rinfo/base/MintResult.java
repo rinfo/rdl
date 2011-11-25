ed85a87bf1a77bbf8c89d44eeb244b3751a41bd7
Niklas Lindström <lindstream@gmail.com>
2011-11-25 21:52:53 +0100

    Add support for explicit CoIN template priority in base

--- packages/java/rinfo-base/src/main/java/se/lagrummet/rinfo/base/MintResult.java
+++ packages/java/rinfo-base/src/main/java/se/lagrummet/rinfo/base/MintResult.java
@@ -6,15 +6,18 @@ public class MintResult {
     private String uri;
     private int matchCount;
     private int rulesSize;
+    private int priority;
 
-    public MintResult(String uri, int matchCount, int rulesSize) {
+    public MintResult(String uri, int matchCount, int rulesSize, int priority) {
         this.uri = uri;
         this.matchCount = matchCount;
         this.rulesSize = rulesSize;
+        this.priority = priority;
     }
 
     public String getUri() { return uri; }
     public Integer getMatchCount() { return matchCount; }
     public Integer getRulesSize() { return rulesSize; }
+    public int getPriority() { return priority; }
 
 }
