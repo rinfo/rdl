76f79ae392553d275b5ad0e1524c84a607275859
Niklas Lindström <lindstream@gmail.com>
2011-11-29 19:10:50 +0100

    Change admin feed generation to extract and use separate entries per org and serie

--- tools/rinfomain/base_as_feed.groovy
+++ tools/rinfomain/base_as_feed.groovy
@@ -14,19 +14,19 @@ import javax.xml.transform.stream.StreamSource
 
 import org.xml.sax.InputSource
 
-import org.apache.commons.io.FileUtils as FU
+import org.apache.commons.io.FileUtils
 import org.apache.commons.io.IOUtils
 
 import org.apache.abdera.Abdera
 import org.apache.abdera.i18n.iri.IRI
 import org.apache.abdera.ext.history.FeedPagingHelper
 
-import org.openrdf.rio.RDFFormat
 import org.openrdf.model.impl.URIImpl
 import org.openrdf.model.vocabulary.RDF
 import org.openrdf.model.vocabulary.OWL
 
 import se.lagrummet.rinfo.base.rdf.RDFUtil
+import se.lagrummet.rinfo.base.rdf.Describer
 
 
 DCT = "http://purl.org/dc/terms/"
@@ -34,7 +34,7 @@ VOID = "http://rdfs.org/ns/void#"
 DCT_HAS_PART = new URIImpl(DCT+"hasPart")
 VOID_DATASET = new URIImpl(VOID+"Dataset")
 
-// TODO: pass xslt dir to script..
+// IMPROVE: pass xslt dir to script..
 GRIT_XSLT = "../../resources/external/xslt/grit/rdfxml-grit.xslt"
 MODEL_XSLT = "../../resources/external/xslt/vocab/grit_to_xhtml.xslt"
 
@@ -44,7 +44,7 @@ FEED_META = [
     publicBaseUri: "http://rinfo.lagrummet.se"
 ]
 
-// FIXME: cumbersome to make it work locally now (when statically served under "/admin" )
+// IMPROVE: cumbersome to make it work locally now (when statically served under "/admin" )
 def feedPathConf(localBase="") {
     return [
         baseUrl: "${localBase}",
@@ -90,7 +90,7 @@ def main() {
         }
         println fpath
         def f = new File(fpath)
-        FU.forceMkdir(f.parentFile)
+        FileUtils.forceMkdir(f.parentFile)
         f.withOutputStream {
             repr.writeTo(it)
         }
@@ -104,28 +104,27 @@ def collectItems(baseUri, base, sources) {
     def addItem = {
         if (it) items << it
     }
+    def listFiles = { dirPath, suffixes ->
+        FileUtils.listFiles(new File(dirPath), suffixes as String[], true)
+    }
 
-    FU.iterateFiles(new File(base, "model"),
-            ["n3"] as String[], true).each {
+    listFiles("${base}/model", ["n3"]).each {
         addItem modelItem(baseUri, it)
     }
 
-    addItem datasetItem(baseUri, "ext/models",
-            FU.listFiles(new File(base, "../external/rdf"),
-                ["rdfs", "owl"] as String[], true))
+    addItem datasetItem(baseUri, "ext/vocab",
+            listFiles("${base}/../external/rdf", ["rdfs", "owl"]))
 
-    // TODO: "ext/model_translations"?
-    addItem datasetItem(baseUri, "ext/extended_modeldata",
-            FU.listFiles(new File(base, "extended/rdf"),
-                ["n3"] as String[], true))
+    addItem datasetItem(baseUri, "ext/vocab/extras",
+            listFiles("${base}/extended/rdf", ["n3"]))
 
-    addItem datasetItem(baseUri, "org",
-            FU.listFiles(new File(base, "datasets/org"),
-                ["n3"] as String[], true))
+    listFiles("${base}/datasets/org", ["n3"]).each {
+        getSimpleItemsByType(baseUri, it).each addItem
+    }
 
-    addItem datasetItem(baseUri, "serie",
-            FU.listFiles(new File(base, "datasets/serie"),
-                ["n3"] as String[], true))
+    listFiles("${base}/datasets/serie", ["n3"]).each {
+        getSimpleItemsByType(baseUri, it).each addItem
+    }
 
     addItem datasetItem(baseUri, "sys/uri", [
             new File(base, "sys/uri/space.n3"),
@@ -139,7 +138,7 @@ def collectItems(baseUri, base, sources) {
         addItem simpleItem(baseUri, "sys/sources", sourceFile)
     } else {
         addItem simpleItem(baseUri, "sys/sources",
-                new File(base, "datasets/feeds.n3"))
+                new File(base, "datasets/sources.n3"))
     }
 
     addItem mediaItem("${baseUri}/media", "Media-filer", [
@@ -240,6 +239,45 @@ def datasetItem(baseUri, uriPath, List<File> files) {
 }
 
 
+def getSimpleItemsByType(String baseUri, File dataFile) {
+    def items = []
+    def repo = RDFUtil.createMemoryRepository()
+    RDFUtil.loadDataFromFile(repo, dataFile)
+    def conn = repo.getConnection()
+    def describer = new Describer(conn).setPrefix("http", "http:")
+    try {
+        def types = describer.objectValues(null, "rdf:type")
+        types.each {
+            describer.getByType(it).each {
+                def iri = it.about
+                assert iri.startsWith(baseUri)
+                def itemRepo = RDFUtil.constructQuery(conn, """CONSTRUCT {
+                            ?item ?p ?o .
+                            ?o ?p2 ?o2 .
+                        } WHERE {
+                            ?item ?p ?o .
+                            OPTIONAL { ?o ?p2 ?o2 . } } """,
+                        [item: conn.valueFactory.createURI(iri)])
+                def itemConn = itemRepo.getConnection()
+                def iter = conn.namespaces
+                while (iter.hasNext()) {
+                    def ns = iter.next()
+                    itemConn.setNamespace(ns.prefix, ns.name)
+                }
+                items << [
+                    uri: iri,
+                    updated: new Date(dataFile.lastModified()),
+                    content: [writeTo: repoWriter(itemRepo),
+                            mediaType: "application/rdf+xml"]
+                ]
+            }
+        }
+    } finally {
+        conn.close()
+    }
+    return items
+}
+
 def mediaItem(itemUri, content, mediaIitems) {
     def youngestEnclDate = null
     def enclosures = []
@@ -284,13 +322,13 @@ Closure createRdfWriter(file, repo=null) {
 
 Closure repoWriter(repo, pretty=true) {
     return {
-        RDFUtil.serialize(repo, RDFFormat.RDFXML.defaultMIMEType, it, true)
+        RDFUtil.serialize(repo, RDFUtil.RDF_XML, it, true)
         //repo.shutDown()
     }
 }
 
 InputStream repoToInStream(repo, pretty=true) {
-    return RDFUtil.toInputStream(repo, RDFFormat.RDFXML.defaultMIMEType, pretty)
+    return RDFUtil.toInputStream(repo, RDFUtil.RDF_XML, pretty)
 }
 
 Closure createFileWriter(file) {
@@ -370,9 +408,6 @@ Map createAtomCollection(feedMeta, feedPathConf, items, markComplete=true) {
         }
 
         for (encl in item.enclosures) {
-            // TODO: when abdera *parses* this (in collector), it seems to
-            // *remove* "mismatch" of fileext and mediaType!
-            // .. cannot reproduce.. Trace..
             def href = feedPathConf.baseUrl +
                     encl.href.replace(".owl", ".rdf").replace(".rdfs", ".rdf")
 
