0268a155cec87b1bec32445d7ab98d7393d52227
Niklas Lindström <lindstream@gmail.com>
2011-12-14 22:23:15 +0100

    Remake RDFChecker to use new SPARQL-construct-based solution

--- packages/java/rinfo-base/src/main/groovy/se/lagrummet/rinfo/base/rdf/checker/RDFChecker.groovy
+++ packages/java/rinfo-base/src/main/groovy/se/lagrummet/rinfo/base/rdf/checker/RDFChecker.groovy
@@ -1,168 +1,55 @@
 package se.lagrummet.rinfo.base.rdf.checker
 
-import java.util.regex.Pattern
-
-import se.lagrummet.rinfo.base.rdf.Describer
-import se.lagrummet.rinfo.base.rdf.Description
-import se.lagrummet.rinfo.base.rdf.RDFLiteral
-import se.lagrummet.rinfo.base.rdf.RDFUtil
-import se.lagrummet.rinfo.base.rdf.Triple
-
+import org.openrdf.query.QueryLanguage
 import org.openrdf.repository.Repository
+import org.openrdf.repository.RepositoryConnection
+import org.openrdf.repository.util.RDFInserter
+
+import se.lagrummet.rinfo.base.rdf.RDFUtil
 
 
 class RDFChecker {
 
-    int smallTextSize = 140
+    Repository repository
+    List<String> testQueries
 
-    protected static RDF_TYPE = Describer.RDF_NS + "type"
+    RDFChecker() {
+        this(RDFUtil.createMemoryRepository())
+    }
 
-    def schemaInfo = new SchemaInfo()
+    RDFChecker(Repository repository) {
+        this.repository = repository
+    }
 
-    Report check(repo, mainUri) {
-        def report = new Report()
-        def describer = new Describer(repo.getConnection())
+    Report check(Repository data, resourceIri, contextIri=resourceIri) {
+        def conn = repository.getConnection()
+        def contextResource = conn.valueFactory.createURI(contextIri)
+        def dataConn = data.getConnection()
         try {
-            // TODO: check all/"descending" resources?
-            def desc = describer.findDescription(mainUri)
-            new ResourceCheck(desc, report).run()
-            if (mainUri != null && desc.getType() == null) {
-                report.add new MissingTypeWarnItem(mainUri)
-            }
+            def inserter =  new RDFInserter(conn)
+            inserter.enforceContext(contextResource)
+            inserter.setPreserveBNodeIDs(false)
+            dataConn.export(inserter)
         } finally {
-            describer.close()
+            dataConn.close()
+        }
+        def report = createReport(conn)
+        try {
+            conn.clear(contextResource)
+        } finally {
+            conn.close()
         }
         return report
     }
 
-
-    class ResourceCheck {
-
-        Description desc
-        Report report
-
-        ResourceCheck(desc, report) {
-            this.desc = desc
-            this.report = report
+    Report createReport(RepositoryConnection conn) {
+        def reportRepo = RDFUtil.createMemoryRepository()
+        def reportConn = reportRepo.getConnection()
+        for (query in testQueries) {
+            def prepQuery = conn.prepareGraphQuery(QueryLanguage.SPARQL, query)
+            prepQuery.evaluate(new RDFInserter(reportConn))
         }
-
-        void run() {
-            checkReference(desc.about, null)
-            for (entry in desc.getPropertyValuesMap().entrySet()) {
-                checkValues(entry.key, entry.value)
-            }
-        }
-
-        void checkValues(property, values) {
-            if (property == RDF_TYPE) {
-                for (value in values) {
-                    checkType(value)
-                }
-                return
-            }
-            def propInfo = schemaInfo.propertyMap.get(property)
-            if (propInfo == null) {
-                report.add new UnknownPropertyWarnItem(property)
-            } else {
-                checkMultiple(propInfo, values)
-            }
-            for (value in values) {
-                checkValue(value, propInfo)
-            }
-        }
-
-        void checkMultiple(propInfo, values) {
-            if (propInfo.requireLang) {
-                checkRequiredLang(propInfo, values)
-            }
-        }
-
-        void checkRequiredLang(propInfo, values) {
-            for (literal in values) {
-                if (literal.lang == propInfo.requireLang) {
-                    return
-                }
-            }
-            report.add new ExpectedLangErrorItem(propInfo.requireLang)
-        }
-
-        void checkValue(Object value, propInfo) {
-            if (value instanceof RDFLiteral) {
-                checkLiteral(value, propInfo)
-            } else {
-                checkReference(value, propInfo)
-            }
-        }
-
-        void checkLiteral(RDFLiteral literal, propInfo) {
-            try {
-                literal.toNativeValue()
-            } catch (e) {
-                report.add new DatatypeErrorItem(literal, e)
-                return
-            }
-            if (propInfo == null)
-                return
-            if (propInfo.reference) {
-                report.add new ExpectedReferenceErrorItem(literal)
-                return
-            }
-            if (propInfo.datatype && propInfo.datatype != literal.datatype) {
-                report.add new UnexpectedDatatypeErrorItem(literal, propInfo.datatype)
-                return
-            }
-            if (propInfo.dateConstraint) {
-                def now = RDFLiteral.toGCal(new Date(), "UTC")
-                if (!propInfo.dateConstraint.verify(literal.toGCal(), now)) {
-                    report.add new DateConstraintWarnItem(
-                            propInfo.dateConstraint, literal, now)
-                }
-                return
-            }
-            if (propInfo.strictWhitespace &&
-                !hasStrictWhitespace(literal.toString())) {
-                report.add new SpuriousWhiteSpaceWarnItem(literal)
-            }
-            checkLexicalPattern(propInfo.lexicalPattern, literal.toString())
-        }
-
-        void checkReference(String uriRef, propInfo) {
-            // NOTE: there's currently no way to enforce a *URI only* ref.
-            if (!uriRef.startsWith("_:")) {
-                try {
-                    new URI(uriRef)
-                } catch (e) {
-                    report.add new MalformedURIRefErrorItem(uriRef, e)
-                }
-            }
-            if (propInfo == null)
-                return
-            if (!propInfo.reference) {
-                report.add new ExpectedLiteralErrorItem(uriRef)
-            }
-            checkLexicalPattern(propInfo.lexicalPattern, uriRef)
-        }
-
-        void checkLexicalPattern(Pattern pattern, String lexical) {
-            if (pattern == null)
-                return
-            if (!pattern.matcher(lexical).matches()) {
-                report.add new PatternMismatchErrorItem(lexical, pattern.toString())
-            }
-        }
-
-        void checkType(String uriRef) {
-            if (!schemaInfo.classes.contains(uriRef)) {
-                report.add new UnknownTypeWarnItem(uriRef)
-            }
-        }
-
-        boolean hasStrictWhitespace(String lexical) {
-            boolean isLongText = (lexical =~ /\n/) || lexical.size() > smallTextSize
-            boolean isNormalized = !(lexical =~ /^\s/ || lexical =~ /\s$/)
-            return isLongText || isNormalized
-        }
-
+        return new Report(reportConn)
     }
 
 }
--- packages/java/rinfo-base/src/main/groovy/se/lagrummet/rinfo/base/rdf/checker/Report.groovy
+++ packages/java/rinfo-base/src/main/groovy/se/lagrummet/rinfo/base/rdf/checker/Report.groovy
@@ -1,179 +1,22 @@
 package se.lagrummet.rinfo.base.rdf.checker
 
+import org.openrdf.repository.RepositoryConnection
 
-class Report {
-    def items = []
-    void add(ReportItem item) {
-        items.add item
-    }
-    boolean isOk() {
-        return items.size() == 0
-    }
-}
+class Report implements Closeable {
 
+    RepositoryConnection connection
 
-// TODO:IMPROVE: store triple for most of these (or bundle in triple + reports)
+    Report(RepositoryConnection conn) {
+        this.connection = conn
+    }
 
-class ReportItem {
-}
+    void close() {
+        connection.close()
+        connection.getRepository().shutDown()
+    }
 
+    boolean isEmpty() {
+        connection.size() == 0
+    }
 
-class WarnItem extends ReportItem {
-}
-
-
-class ErrorItem extends ReportItem {
-}
-
-
-class DatatypeErrorItem extends ErrorItem {
-    def literal
-    def error
-    DatatypeErrorItem(literal, error) {
-        this.literal = literal
-        this.error = error
-    }
-    String toString() {
-        return "DatatypeErrorItem: " +
-            " literal = " + literal + ";"
-            " error = " + error + ";"
-    }
-}
-
-
-class MissingTypeWarnItem extends WarnItem {
-    String uriRef
-    MissingTypeWarnItem(uriRef) {
-        this.uriRef = uriRef
-    }
-    String toString() {
-        return "MissingTypeWarnItem: " +
-            " uriRef = " + uriRef + ";"
-    }
-}
-
-class MalformedURIRefErrorItem extends ErrorItem {
-    String uriRef
-    def error
-    MalformedURIRefErrorItem(uriRef, error) {
-        this.uriRef = uriRef
-        this.error = error
-    }
-    String toString() {
-        return "MalformedURIRefErrorItem: " +
-            " uriRef = " + uriRef + ";"
-            " error = " + error + ";"
-    }
-}
-
-
-class UnknownTypeWarnItem extends WarnItem {
-    String uriRef
-    UnknownTypeWarnItem(uriRef) {
-        this.uriRef = uriRef
-    }
-    String toString() {
-        return "UnknownTypeWarnItem: " +
-            " uriRef = " + uriRef + ";"
-    }
-}
-
-class UnknownPropertyWarnItem extends WarnItem {
-    String uriRef
-    UnknownPropertyWarnItem(uriRef) {
-        this.uriRef = uriRef
-    }
-    String toString() {
-        return "UnknownPropertyWarnItem: " +
-            " uriRef = " + uriRef + ";"
-    }
-}
-
-class ExpectedReferenceErrorItem extends ErrorItem {
-    def literal
-    ExpectedReferenceErrorItem(literal) {
-        this.literal = literal
-    }
-    String toString() {
-        return "ExpectedReferenceErrorItem: " +
-            " literal = " + literal + ";"
-    }
-}
-
-class ExpectedLiteralErrorItem extends ErrorItem {
-    def uriRef
-    ExpectedLiteralErrorItem(uriRef) {
-        this.uriRef = uriRef
-    }
-    String toString() {
-        return "ExpectedLiteralErrorItem: " +
-            " uriRef = " + uriRef + ";"
-    }
-}
-
-class UnexpectedDatatypeErrorItem extends ErrorItem {
-    def literal
-    def expected
-    UnexpectedDatatypeErrorItem(literal, expected) {
-        this.literal = literal
-        this.expected = expected
-    }
-    String toString() {
-        return "UnexpectedDatatypeErrorItem: " +
-            " literal = " + literal + ";"
-            " expected = " + expected + ";"
-    }
-}
-
-class ExpectedLangErrorItem extends ErrorItem {
-    def expected
-    ExpectedLangErrorItem(expected) {
-        this.expected = expected
-    }
-    String toString() {
-        return "ExpectedLangErrorItem: " +
-            " expected = " + expected + ";"
-    }
-}
-
-class PatternMismatchErrorItem extends ErrorItem {
-    def lexical
-    def pattern
-    PatternMismatchErrorItem(lexical, pattern) {
-        this.lexical = lexical
-        this.pattern = pattern
-    }
-    String toString() {
-        return "PatternMismatchErrorItem: " +
-            " lexical = " + lexical + ";"
-            " pattern = " + pattern + ";"
-    }
-}
-
-class SpuriousWhiteSpaceWarnItem extends WarnItem {
-    def literal
-    SpuriousWhiteSpaceWarnItem(literal) {
-        this.literal = literal
-    }
-    String toString() {
-        return "SpuriousWhiteSpaceWarnItem: " +
-            " literal = " + literal + ";"
-    }
-}
-
-class DateConstraintWarnItem extends WarnItem {
-    def constraint
-    def literal
-    def now
-    DateConstraintWarnItem(constraint, literal, now) {
-        this.constraint = constraint
-        this.literal = literal
-        this.now = now
-    }
-    String toString() {
-        return "DateConstraintWarnItem: " +
-            " constraint = " + constraint + ";"
-            " literal = " + literal + ";"
-            " now = " + now + ";"
-    }
 }
