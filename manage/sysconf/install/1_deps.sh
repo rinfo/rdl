f414216bbe38f11d500f79ce5927f6a1b114bcad
Niklas Lindström <lindstream@gmail.com>
2012-04-03 17:31:38 +0200

    Add some software in manage/sysconf/install/1_deps.sh

--- manage/sysconf/install/1_deps.sh
+++ manage/sysconf/install/1_deps.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt-get update
+
 # web server tools:
 sudo apt-get install apache2 -y
 sudo /usr/sbin/a2enmod proxy_ajp
@@ -8,6 +10,10 @@ sudo apt-get install pgp -y
 
 # admin tools:
 sudo apt-get install wget
+sudo apt-get install curl
 sudo apt-get install unzip
 sudo apt-get install lsof
+
+# java
+sudo apt-get install openjdk-6-jdk -y
 
