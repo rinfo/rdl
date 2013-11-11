a52e81c30a115426cdbfd7a4ce46d3cf8f0c870b
cheijken <christian.heijkenskjold@gmail.com>
2013-11-11 15:30:03 +0100

    Improve base install scripts

--- manage/prepare_linux_install_machine.sh
+++ manage/prepare_linux_install_machine.sh
@@ -2,11 +2,11 @@
 # Copy the contents of this script into a shell script file and remember to make it executable. Then run it.
 # Will create subdirectory rinfo and checkout develop branch or selected version/feature
 
-apt-get update
-apt-get install git -y
-apt-get install build-essential python-dev python-pkg-resources python-setuptools sudo -y
-easy_install pip
-pip install fabric
+sudo apt-get update
+sudo apt-get install git -y
+sudo apt-get install build-essential python-dev python-pkg-resources python-setuptools sudo -y
+sudo easy_install pip
+sudo pip install fabric
 
 mkdir rinfo
 cd rinfo
