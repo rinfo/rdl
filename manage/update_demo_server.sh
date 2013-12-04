44b99f5779bd40e6ddbf18903cf3ed3b92946655
cheijken <christian.heijkenskjold@gmail.com>
2013-12-04 10:54:43 +0100

    Change password support

--- manage/update_demo_server.sh
+++ manage/update_demo_server.sh
@@ -1,2 +1,2 @@
-fab -p $1 target.demo app.admin.all app.main.all app.checker.all app.service.all server.restart_tomcat
+fab -p $PW_RINFO target.demo app.admin.all app.main.all app.checker.all app.service.all server.restart_tomcat
 
