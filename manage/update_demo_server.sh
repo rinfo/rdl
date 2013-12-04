d31251f4a70c2810dc29b2a4cb9efc4b1a5b12a8
cheijken <christian.heijkenskjold@gmail.com>
2013-12-04 11:02:03 +0100

    Improve tomcat restart

--- manage/update_demo_server.sh
+++ manage/update_demo_server.sh
@@ -1,2 +1,4 @@
-fab -p $PW_RINFO target.demo app.admin.all app.main.all app.checker.all app.service.all server.restart_tomcat
+fab -p $PW_RINFO target.demo app.admin.all app.main.all app.checker.all app.service.all
+fab -p $PW_RINFO target.demo -R main server.restart_tomcat
+
 
