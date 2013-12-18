5b4decd940d52647fa0f17f1e12f9dd0efe8d967
cheijken <christian.heijkenskjold@gmail.com>
2013-12-18 10:54:54 +0100

    Ask for password if not supplied

--- manage/update_demo_server.sh
+++ manage/update_demo_server.sh
@@ -1,3 +1,8 @@
+if [ -z "$PW_RINFO" ]; then
+	echo "Enter sudo password: "
+	read PW_RINFO
+fi
+
 fab -p $PW_RINFO target.demo app.admin.all app.main.all app.checker.all app.service.all
 fab -p $PW_RINFO target.demo -R main server.restart_tomcat
 
