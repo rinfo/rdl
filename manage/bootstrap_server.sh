6e2d529760a0c4ec7e6ec2aa8f3884109f7df0bf
cheijken <christian.heijkenskjold@gmail.com>
2014-03-10 11:01:20 +0100

    Prefere ipv4, to speed up debian updates

--- manage/bootstrap_server.sh
+++ manage/bootstrap_server.sh
@@ -4,12 +4,16 @@
 # Install rinfo user and sudo
 ssh root@$1 apt-get install sudo -y 
 ssh root@$1 useradd rinfo -m -G sudo -s /bin/bash
-ssh root@$1 passwd rinfo 
+ssh root@$1 passwd rinfo
+
+# Prefere ipv4, to speed up debian updates
+ssh root@$1 'echo "precedence ::ffff:0:0/96  100" >>  /etc/gai.conf'
 
 # Automate login
 ssh rinfo@$1 mkdir .ssh
 scp ~/.ssh/id_rsa.pub rinfo@$1:.
 ssh rinfo@$1 'cat id_rsa.pub >> .ssh/authorized_keys'
 ssh rinfo@$1 rm id_rsa.pub
+
 
 
