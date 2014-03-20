872562289cdd5b003f8813d389051d05eb66ec8e
cheijken <christian.heijkenskjold@gmail.com>
2014-03-20 16:19:16 +0100

    Add install script for regression server base

---
+++ manage/install_regression_server.sh
@@ -0,0 +1,11 @@
+# Installs all necessary compontents for a server to act as a regression test server 
+
+echo "Enter sudo password: "
+read pwd
+
+fab -p $pwd target.regression -R main,service,checker,admin sysconf.install_server sysconf.configure_server
+fab -p $pwd target.regression -R main,service,checker,admin app.service.install_elasticsearch
+fab -p $pwd target.regression -R main,service,checker,admin app.service.start_elasticsearch
+fab -p $pwd target.regression -R main,service,checker,admin app.service.deploy_sesame
+
+
