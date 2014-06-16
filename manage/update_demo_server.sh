47c1f1fb61bb3dade44409a5287d99f3676d0189
cheijken <christian.heijkenskjold@gmail.com>
2014-06-16 13:47:17 +0200

    Add tomcat restart to update demo script. #124

--- manage/update_demo_server.sh
+++ manage/update_demo_server.sh
@@ -9,7 +9,7 @@ START_TIME=$SECONDS
 #fab -p $PW_RINFO target.demo app.admin.all app.main.all app.checker.all app.service.all
 #fab -p $PW_RINFO target.demo app.admin.all app.main.all:deps=0,test=0 app.checker.all:deps=0,test=0 app.service.all:deps=0,test=0
 fab -p $PW_RINFO target.demo app.admin.all app.main.all:deps=0 app.checker.all:deps=0 app.service.all:deps=0
-#fab -p $PW_RINFO target.demo -R main server.restart_tomcat
+fab -p $PW_RINFO target.demo -R main server.restart_tomcat
 
 ELAPSED_TIME=$((SECONDS-START_TIME))
 
