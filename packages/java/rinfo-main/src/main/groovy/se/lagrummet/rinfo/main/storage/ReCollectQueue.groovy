0ea629c7729066792554c3d28cc51381908e95cb
Jon Eldeklint <jon@eldeklint.se>
2014-08-27 15:57:43 +0200

    Adds logging to the recollectqueue #84

--- packages/java/rinfo-main/src/main/groovy/se/lagrummet/rinfo/main/storage/ReCollectQueue.groovy
+++ packages/java/rinfo-main/src/main/groovy/se/lagrummet/rinfo/main/storage/ReCollectQueue.groovy
@@ -1,18 +1,28 @@
 package se.lagrummet.rinfo.main.storage
-import org.apache.abdera.model.Entry;
+import org.apache.abdera.model.Entry
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory;
 //use with MyClass singleton = MyClass.instance
 @Singleton
 class ReCollectQueue {
+    private final Logger logger = LoggerFactory.getLogger(ReCollectQueue.class)
+
     private def failedQueue = [] as Queue
     public boolean isEmpty() {
         return failedQueue.isEmpty()
     }
     public def add(FailedEntry failedEntry) {
+        if(!failedEntry.contentEntry) {
+            logger.error("there MUST be an entry to recollect.")
+            return
+        }
+        logger.debug("Adding ${failedEntry.contentEntry?.id}")
         if(!failedQueue.any { it.contentEntry?.id == failedEntry.contentEntry.id})
             failedQueue << failedEntry
         else {
             def failed = failedQueue.find {it.contentEntry.id == failedEntry.contentEntry.id } as FailedEntry
             failed.numberOfRetries += 1
+            logger.debug("${failedEntry.contentEntry?.id} already exists in queue, increasing number of tries (now ${failed.numberOfRetries}) and requeue")
             failedQueue.removeAll { it.contentEntry.id == failed.contentEntry.id }
             failedQueue << failed
         }
@@ -36,11 +46,18 @@ class ReCollectQueue {
     }
 
     public def purgeQueue() {
+        logger.info("Purging the whole recollect queue")
         return failedQueue.removeAll(failedQueue)
     }
 
     private def applyLimit() {
-        failedQueue.removeAll { it.numberOfRetries > 3}
+        failedQueue.removeAll {
+            if(it.numberOfRetries > 3) {
+                logger.info("Too many retries, removing ${it.contentEntry.id} from the queue")
+                return true
+            }
+            return false
+        }
     }
 
 
