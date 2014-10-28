6d808567b92da445e8948b37f64c64edf0d424ad
Jon Eldeklint <jon@eldeklint.se>
2014-10-28 16:12:33 +0100

    Adds exitcode check in the updatescripts #145

--- manage/update_demo_server.sh
+++ manage/update_demo_server.sh
@@ -9,6 +9,13 @@ START_TIME=$SECONDS
 #fab -p $PW_RINFO target.demo app.admin.all app.main.all app.checker.all app.service.all
 #fab -p $PW_RINFO target.demo app.admin.all app.main.all:deps=0,test=0 app.checker.all:deps=0,test=0 app.service.all:deps=0,test=0
 fab -p $PW_RINFO target.demo app.admin.all app.main.all:deps=0 app.checker.all:deps=0 app.service.all:deps=0
+
+EXIT_STATUS=$?
+if [ $EXIT_STATUS -ne 0 ];then
+   echo "fabric returned $EXIT_STATUS! Exiting!"
+   exit $EXIT_STATUS
+fi
+
 fab -p $PW_RINFO target.demo -R main server.restart_tomcat
 
 ELAPSED_TIME=$((SECONDS-START_TIME))
--- manage/update_docs.sh
+++ manage/update_docs.sh
@@ -5,3 +5,9 @@ if [ -z "$PW_RINFO" ]; then
 fi
 
 fab -p $PW_RINFO target.prod -R doc app.docs.build app.docs.deploy
+
+EXIT_STATUS=$?
+if [ $EXIT_STATUS -ne 0 ];then
+   echo "fabric returned $EXIT_STATUS! Exiting!"
+   exit $EXIT_STATUS
+fi
