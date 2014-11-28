3b06babb30c614a47d8e8468cdadbfcbd9987b70
Kåre Jonsson <kare.jonsson@dom.se>
2014-11-28 15:10:47 +0100

    Mode and ownerships on exposed files

--- packageinstall/admin/install.sh
+++ packageinstall/admin/install.sh
@@ -14,15 +14,18 @@ apt-get install apache2 -y
 /etc/init.d/ssh restart
 mv robots.txt /var/www/
 chmod u=rw,a=r /var/www/robots.txt
+chown root:root /var/www/robots.txt
 
 mv jk.conf /etc/apache2/conf.d/
 
-mv admin > /etc/apache2/sites-available/admin
+mv admin /etc/apache2/sites-available/admin
+
+mkdir -p /var/www/admin
 ( cd output && cp -r * /var/www/admin/ )
 chmod -R 755 /var/www/admin
-chown rinfo:root /var/www/admin
+chown -R rinfo:root /var/www/admin
 
-chown rinfo:rinfo /var/www/admin/*
+chown -R rinfo:rinfo /var/www/admin/*
 
 a2ensite admin
 
