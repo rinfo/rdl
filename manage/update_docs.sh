ce60b6e6c060bd951f1cd021f59d2dd09c236378
cheijken <christian.heijkenskjold@gmail.com>
2015-01-21 11:03:10 +0100

    Fix scripts

--- manage/update_docs.sh
+++ manage/update_docs.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-if [ -z "$PW_RINFO" ]; then
-        echo "Enter sudo password: "
-        read PW_RINFO
-fi
 
-fab -p $PW_RINFO target.prod -R doc app.docs.build app.docs.deploy
+fab target.prod -R doc app.docs.build app.docs.deploy
 
 EXIT_STATUS=$?
 if [ $EXIT_STATUS -ne 0 ];then
