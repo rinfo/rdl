54ad7eab19f610ceabca5a05ee171afa3c28872c
cheijken <christian.heijkenskjold@gmail.com>
2015-02-27 11:10:07 +0100

    Refactor. #152

--- packages/java/rinfo-service/src/main/groovy/se/lagrummet/rinfo/service/elasticsearch/impl/ElasticQueryBuilderQueryBuilder.groovy
+++ packages/java/rinfo-service/src/main/groovy/se/lagrummet/rinfo/service/elasticsearch/impl/ElasticQueryBuilderQueryBuilder.groovy
@@ -18,7 +18,7 @@ import se.lagrummet.rinfo.service.elasticsearch.RDLQueryBuilder
  */
 class ElasticQueryBuilderQueryBuilder implements RDLQueryBuilder.QueryBuilder {
 
-    BoolQueryBuilder boolQuery
+    BoolQueryBuilderExplained boolQueryBuilderExplained
     private RDLQueryBuilderImpl rdlQueryBuilder
     private int page
     private int pageSize
@@ -26,69 +26,18 @@ class ElasticQueryBuilderQueryBuilder implements RDLQueryBuilder.QueryBuilder {
 
     ElasticQueryBuilderQueryBuilder(RDLQueryBuilderImpl rdlQueryBuilder) {
         this.rdlQueryBuilder = rdlQueryBuilder
-        boolQuery = QueryBuilders.boolQuery()
+        boolQueryBuilderExplained = new BoolQueryBuilderExplained()
 
     }
 
     @Override
-    void addQuery(String querytext) {
-        addSearchQueryForPreSelectedSearchFields(querytext, RDLQueryBuilder.QUERY_SEARCH_FIELDS, RDLQueryBuilder.QUERY_MINIMUM_MATCH)
+    void addQuery(String queryText) {
+        boolQueryBuilderExplained.addSearchQueryForPreSelectedSearchFields(queryText, RDLQueryBuilder.QUERY_SEARCH_FIELDS, RDLQueryBuilder.QUERY_MINIMUM_MATCH)
     }
 
     @Override
     void restrictType(String type) {
         types.add(type)
-    }
-
-    @Override
-    RDLQueryBuilder.Result result(String iriReplaceUrl) {
-        boostTypeInSearch("KonsolideradGrundforfattning", RDLQueryBuilder.TYPE_BOOST_KONSOLIDERAD_GRUNDFORFATTNING)
-
-        SearchRequestBuilder searchRequestBuilder = createAndPrepareSearchRequestBuilder()
-
-        return new ElasticQueryBuilderResult(response: searchRequestBuilder.execute().actionGet(), iriReplaceUrl: iriReplaceUrl, page:page, pageSize:pageSize)
-    }
-
-    private SearchRequestBuilder createAndPrepareSearchRequestBuilder() {
-        SearchRequestBuilder searchRequestBuilder = rdlQueryBuilder.prepareSearch()
-
-        calculatePagination(searchRequestBuilder)
-        setHighlightedFields(searchRequestBuilder, RDLQueryBuilder.HIGHLIGHTERS_TAG, RDLQueryBuilder.HIGHLIGHTED_FIELDS)
-        searchRequestBuilder.addFields(RDLQueryBuilder.SELECT_FIELDS.tokenize(',').collect {it.trim()} as String[] )
-
-        searchRequestBuilder.setQuery(reduceScoreFilterForMultipleTitlesOnQuery(boolQuery))
-
-        if (!types.isEmpty()) {
-            BoolFilterBuilder filter = addFilterForTypes("type",types);
-            searchRequestBuilder.setPostFilter(filter)
-        }
-
-        prepareGroupResultByType(searchRequestBuilder)
-
-        return searchRequestBuilder
-    }
-
-    private static SearchRequestBuilder prepareGroupResultByType(SearchRequestBuilder searchRequestBuilder) {
-        searchRequestBuilder.addAggregation(
-                AggregationBuilders
-                        .filters("byType")
-                        .filter("Lagar",createOrFilterByGroup("Lagar"))
-                        .filter("Rattsfall",createOrFilterByGroup("Rattsfall"))
-                .subAggregation(
-                        setHighlightedFields(
-                            AggregationBuilders.topHits("top")
-                                    .setFetchSource(RDLQueryBuilder.SELECT_FIELDS.tokenize(',').collect {it.trim()} as String[])
-                                    .setSize(4)
-                            ,RDLQueryBuilder.HIGHLIGHTERS_TAG
-                            ,RDLQueryBuilder.HIGHLIGHTED_FIELDS
-                        ) as AbstractAggregationBuilder
-                )
-        )
-
-    }
-
-    static OrFilterBuilder createOrFilterByGroup(String group) {
-        FilterBuilders.orFilter(RDLQueryBuilder.TYPE.findAll { it.group==group }.collect { FilterBuilders.termFilter("type", it.type) } as org.elasticsearch.index.query.FilterBuilder[])
     }
 
     @Override
@@ -102,12 +51,54 @@ class ElasticQueryBuilderQueryBuilder implements RDLQueryBuilder.QueryBuilder {
     @Override
     void close() {}
 
-    private BoolQueryBuilder addSearchQueryForPreSelectedSearchFields(String queryText, String[] searchFields, String minimalMatchPercent) {
-        boolQuery.should(
-                QueryBuilders.multiMatchQuery(queryText, searchFields)
-                        .type(MatchQuery.Type.PHRASE_PREFIX)
-                        .minimumShouldMatch(minimalMatchPercent)
+    @Override
+    RDLQueryBuilder.Result result(String iriReplaceUrl) {
+        SearchRequestBuilder searchRequestBuilder = createAndPrepareSearchRequestBuilder()
+
+        return new ElasticQueryBuilderResult(response: searchRequestBuilder.execute().actionGet(), iriReplaceUrl: iriReplaceUrl, page:page, pageSize:pageSize)
+    }
+
+    private SearchRequestBuilder createAndPrepareSearchRequestBuilder() {
+        SearchRequestBuilder searchRequestBuilder = rdlQueryBuilder.prepareSearch()
+
+        calculatePagination(searchRequestBuilder)
+        setHighlightedFields(searchRequestBuilder, RDLQueryBuilder.HIGHLIGHTERS_TAG, RDLQueryBuilder.HIGHLIGHTED_FIELDS)
+        searchRequestBuilder.addFields(RDLQueryBuilder.SELECT_FIELDS.tokenize(',').collect {it.trim()} as String[] )
+
+        boolQueryBuilderExplained.boostTypeInSearch("KonsolideradGrundforfattning", RDLQueryBuilder.TYPE_BOOST_KONSOLIDERAD_GRUNDFORFATTNING)
+        boolQueryBuilderExplained.imposeTo(searchRequestBuilder)
+
+        if (!types.isEmpty())
+            searchRequestBuilder.setPostFilter(addFilterForTypes("type",types))
+
+        prepareGroupResultByType(searchRequestBuilder)
+
+        return searchRequestBuilder
+    }
+
+    /* Helper functions */
+
+    private static OrFilterBuilder createOrFilterByGroup(String group) {
+        FilterBuilders.orFilter(RDLQueryBuilder.TYPE.findAll { it.group==group }.collect { FilterBuilders.termFilter("type", it.type) } as org.elasticsearch.index.query.FilterBuilder[])
+    }
+
+    private static SearchRequestBuilder prepareGroupResultByType(SearchRequestBuilder searchRequestBuilder) {
+        searchRequestBuilder.addAggregation(
+                AggregationBuilders
+                        .filters("byType")
+                        .filter("Lagar",createOrFilterByGroup("Lagar"))
+                        .filter("Rattsfall",createOrFilterByGroup("Rattsfall"))
+                        .subAggregation(
+                        setHighlightedFields(
+                                AggregationBuilders.topHits("top")
+                                        .setFetchSource(RDLQueryBuilder.SELECT_FIELDS.tokenize(',').collect {it.trim()} as String[])
+                                        .setSize(4)
+                                ,RDLQueryBuilder.HIGHLIGHTERS_TAG
+                                ,RDLQueryBuilder.HIGHLIGHTED_FIELDS
+                        ) as AbstractAggregationBuilder
+                )
         )
+
     }
 
     private static BoolFilterBuilder addFilterForTypes(String terms, def types) {
@@ -119,25 +110,11 @@ class ElasticQueryBuilderQueryBuilder implements RDLQueryBuilder.QueryBuilder {
     }
 
 
-    private BoolQueryBuilder boostTypeInSearch(String type, float boostValue) {
-        boolQuery.should(
-                QueryBuilders.constantScoreQuery(FilterBuilders.termFilter("type", type))
-                        .boost(boostValue)
-        )
-    }
-
     private SearchRequestBuilder calculatePagination(SearchRequestBuilder searchRequestBuilder) {
         searchRequestBuilder.setFrom(startIndex())
         searchRequestBuilder.setSize(pageSize)
     }
 
-    static private def reduceScoreFilterForMultipleTitlesOnQuery(def query) {
-        QueryBuilders.functionScoreQuery(query as QueryBuilder,
-            ScoreFunctionBuilders.scriptFunction(
-                "if(_source.title instanceof List) { 1/pow(3, _source.title.size()) } else { 1 }"
-            )
-        )
-    }
 
     private static def setHighlightedFields(def searchRequestBuilder, highlighters_tag, highlighted_fields) {
         searchRequestBuilder.setHighlighterPreTags(highlighters_tag.start)
@@ -148,4 +125,41 @@ class ElasticQueryBuilderQueryBuilder implements RDLQueryBuilder.QueryBuilder {
         return searchRequestBuilder
     }
 
+    /* Helper classes */
+
+    private static class BoolQueryBuilderExplained {
+        BoolQueryBuilder boolQuery = QueryBuilders.boolQuery()
+
+        BoolQueryBuilderExplained() {
+            reduceScoreFilterForMultipleTitlesOnQuery()
+        }
+
+        BoolQueryBuilder addSearchQueryForPreSelectedSearchFields(String queryText, String[] searchFields, String minimalMatchPercent) {
+            boolQuery.should(
+                    QueryBuilders.multiMatchQuery(queryText, searchFields)
+                            .type(MatchQuery.Type.PHRASE_PREFIX)
+                            .minimumShouldMatch(minimalMatchPercent)
+            )
+        }
+
+        BoolQueryBuilder boostTypeInSearch(String type, float boostValue) {
+            boolQuery.should(
+                    QueryBuilders.constantScoreQuery(FilterBuilders.termFilter("type", type))
+                            .boost(boostValue)
+            )
+        }
+
+        private def reduceScoreFilterForMultipleTitlesOnQuery() {
+            QueryBuilders.functionScoreQuery(boolQuery,
+                    ScoreFunctionBuilders.scriptFunction(
+                            "if(_source.title instanceof List) { 1/pow(3, _source.title.size()) } else { 1 }"
+                    )
+            )
+        }
+
+        void imposeTo(SearchRequestBuilder searchRequestBuilder) {
+            searchRequestBuilder.setQuery(boolQuery)
+        }
+
+    }
 }
