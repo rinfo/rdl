30ca60e8d6f8c89266d7d741a845894d489ca138
cheijken <christian.heijkenskjold@gmail.com>
2015-05-06 15:58:28 +0200

    Fix truncate symptoms, but not real problem. #197

--- packages/java/rinfo-service/src/main/groovy/se/lagrummet/rinfo/service/elasticsearch/impl/Utils.groovy
+++ packages/java/rinfo-service/src/main/groovy/se/lagrummet/rinfo/service/elasticsearch/impl/Utils.groovy
@@ -3,9 +3,8 @@ package se.lagrummet.rinfo.service.elasticsearch.impl
 import org.elasticsearch.action.search.SearchResponse
 import org.elasticsearch.search.SearchHit
 import org.elasticsearch.search.SearchHits
-import org.elasticsearch.search.aggregations.bucket.terms.Terms
-import org.elasticsearch.search.aggregations.metrics.tophits.TopHits
 import org.elasticsearch.search.facet.datehistogram.DateHistogramFacet
+import se.lagrummet.rinfo.service.elasticsearch.ElasticSearchQueryBuilder
 
 /**
  * Created by christian on 2/18/15.
@@ -75,10 +74,43 @@ class Utils {
             item.describedby = makeServiceLink(item.iri, iriReplaceUrl)
         }
         hit.highlightFields.each { key, hlf ->
-            item.get('matches', [:])[key] = hlf.fragments.collect { it.toString() }
+            //todo improve this solution to manually fix truncations when Elastic limit of 150 for some reason fails. see truncate(v)
+            item.get('matches', [:])[key] = hlf.fragments.collect { def v = it.toString(); return v.length() > 180 ? truncate(v) : v }
         }
         item.explanation = hit.getExplanation()?.toHtml()
         return item
+    }
+
+    static def truncate(String v) {
+        int startOfHighlighter = v.indexOf(ElasticSearchQueryBuilder.HIGHLIGHTERS_TAG.start)
+        int endOfHighlighter = v.lastIndexOf(ElasticSearchQueryBuilder.HIGHLIGHTERS_TAG.end)
+
+        if (startOfHighlighter==-1 || endOfHighlighter == -1)
+            return v
+        int calculatedLastPartBehindHighlighter = v.length() - ( endOfHighlighter + ElasticSearchQueryBuilder.HIGHLIGHTERS_TAG.end.length() )
+        if (calculatedLastPartBehindHighlighter < 0)
+            return v
+
+        String start = v.substring(0,startOfHighlighter)
+        String mid = v.substring(startOfHighlighter, endOfHighlighter + ElasticSearchQueryBuilder.HIGHLIGHTERS_TAG.end.length())
+        String end = v.substring(endOfHighlighter + ElasticSearchQueryBuilder.HIGHLIGHTERS_TAG.end.length())
+
+        int startLength = start.length()
+        int endLength = end.length()
+        if (startLength+endLength<=150)
+            return v
+
+        if (startLength > 150) startLength = 150;
+        if (endLength > 150) endLength = 150;
+
+        if (startLength>=endLength) {
+            endLength = endLength / 2
+            startLength -= endLength
+        } else {
+            startLength = startLength / 2
+            endLength -= startLength
+        }
+        return start.substring(start.length() - startLength) + mid + end.substring(0, endLength)
     }
 
     static String makeServiceLink(String iri, String iriReplaceUrl) {
